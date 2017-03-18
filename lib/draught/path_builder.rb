1a03fc631cc6dd3efa90ed318fbb0c5350562232
Matt Patterson <matt@reprocessed.org>
2017-03-18 19:27:34 +0100

    Allow PathBuilder to connect multiple paths
    Bascially allowing easy connection of paths which all begin at 0,0 into
    a single path which behaves in an unsurprising way: the second path is
    translated relative to the last point of first path, and the third path
    segment translated relative to the last point of the new
    first-and-second-segments path, and so on.

--- lib/draught/path_builder.rb
+++ lib/draught/path_builder.rb
@@ -8,6 +8,16 @@ module Draught
       builder.send(:path)
     end
 
+    def self.connect(*paths_and_points)
+      build { |p|
+        p << paths_and_points[0]
+        paths_and_points[1..-1].inject(p.last) { |point, path_or_point|
+          p << path_or_point.translate(point)
+          p.last
+        }
+      }
+    end
+
     attr_reader :path
     private :path
 
@@ -17,6 +27,7 @@ module Draught
 
     def <<(path_or_point)
       @path = path << path_or_point
+      self
     end
 
     def last
--- spec/draught/path_builder_spec.rb
+++ spec/draught/path_builder_spec.rb
@@ -15,6 +15,14 @@ module Draught
       expect(path.points).to eq([p1, p2])
     end
 
+    it "allows chaining << as Array does" do
+      path = PathBuilder.build { |p|
+        p << p1 << p2
+      }
+
+      expect(path.points).to eq([p1, p2])
+    end
+
     specify "the yielded value will return the last point added if asked" do
       collector = []
       path = PathBuilder.build { |p|
@@ -39,6 +47,30 @@ module Draught
       }
 
       expect(collector).to eq([p2])
+    end
+
+    describe "connecting several paths together into a new path each relative to the last" do
+      let(:horizontal) { PathBuilder.build { |p| p << Point.new(0,0) << Point.new(1,0) } }
+      let(:diagonal) { PathBuilder.build { |p| p << Point.new(0,0) << Point.new(1,1) } }
+      let(:spaced_horizontal) { PathBuilder.build { |p|
+        p << Point.new(2,0) << Point.new(3,0) }
+      }
+
+      it "connects as expected" do
+        path = PathBuilder.connect(horizontal, spaced_horizontal)
+
+        expect(path.points).to eq([
+          Point.new(0,0), Point.new(1,0), Point.new(3,0), Point.new(4,0)
+        ])
+      end
+
+      it "will also connect in bare Points as expected" do
+        path = PathBuilder.connect(horizontal, Point.new(1,0), spaced_horizontal)
+
+        expect(path.points).to eq([
+          Point.new(0,0), Point.new(1,0), Point.new(2,0), Point.new(4,0), Point.new(5,0)
+        ])
+      end
     end
   end
 end
