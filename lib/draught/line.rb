bbf0dda0a54e8a06813c775ff09d88fc758994c3
Matt Patterson <matt@reprocessed.org>
2017-05-10 17:43:54 +0200

    Create Lines from angle + length
    Also enable Lines created from points to figure out what angles they are
    at.

--- lib/draught/line.rb
+++ lib/draught/line.rb
@@ -1,15 +1,154 @@
 require_relative './path'
 require_relative './point'
+require_relative './transformations'
 
 module Draught
   class Line
+    DEGREES_90 = Math::PI / 2
+    DEGREES_180 = Math::PI
+    DEGREES_270 = Math::PI * 1.5
+    DEGREES_360 = Math::PI * 2
+
     class << self
       def horizontal(width)
-        Path.new([Point::ZERO, Point.new(width, 0)])
+        build(end_point: Point.new(width, 0)).path
       end
 
       def vertical(height)
-        Path.new([Point::ZERO, Point.new(0, height)])
+        build(end_point: Point.new(0, height)).path
+      end
+
+      def build(args = {})
+        builder_class = args.has_key?(:end_point) ? LineBuilderFromPoint : LineBuilderFromAngles
+        line_args = builder_class.new(args).line_args
+        new(line_args)
+      end
+    end
+
+    attr_reader :end_point, :length, :radians
+
+    def initialize(args)
+      @end_point = args.fetch(:end_point)
+      @length = args.fetch(:length)
+      @radians = args.fetch(:radians)
+    end
+
+    def path
+      @path ||= Path.new([Point::ZERO, end_point])
+    end
+
+    class LineBuilderFromAngles
+      attr_reader :length, :radians
+      private :length, :radians
+
+      def initialize(args)
+        @length = args.fetch(:length)
+        @radians = args.fetch(:radians)
+      end
+
+      def line_args
+        {length: length, radians: radians, end_point: end_point}
+      end
+
+      private
+
+      def end_point
+        hardwired_end_points.fetch(restricted_radians) {
+          single_quadrant_end_point.transform(Transformations.rotate(remaining_angle))
+        }
+      end
+
+      def restricted_radians
+        @restricted_radians ||= restrict_to_360_degrees(radians)
+      end
+
+      def restrict_to_360_degrees(radians)
+        radians % DEGREES_360
+      end
+
+      def hardwired_end_points
+        {
+          0 => Point.new(length,0),
+          DEGREES_90 => Point.new(0,length),
+          DEGREES_180 => Point.new(-length,0),
+          DEGREES_270 => Point.new(0,-length),
+          DEGREES_360 => Point.new(length,0)
+        }
+      end
+
+      def single_quadrant_end_point
+        Point.new(x, y)
+      end
+
+      def x
+        Math.cos(single_quadrant_angle) * length
+      end
+
+      def y
+        Math.sin(single_quadrant_angle) * length
+      end
+
+      def single_quadrant_angle
+        @single_quadrant_angle ||= restricted_radians - remaining_angle
+      end
+
+      def remaining_angle
+        @remaining_angle ||= begin
+          [DEGREES_270, DEGREES_180, DEGREES_90, 0].find { |angle|
+            restricted_radians > angle
+          } || 0
+        end
+      end
+    end
+
+    class LineBuilderFromPoint
+      attr_reader :end_point
+      private :end_point
+
+      def initialize(args)
+        @end_point = args.fetch(:end_point)
+      end
+
+      def line_args
+        {length: length, radians: radians, end_point: end_point}
+      end
+
+      private
+
+      def length
+        @length ||= Math.sqrt(x_length ** 2 + y_length ** 2)
+      end
+
+      def radians
+        @radians ||= angle_to_start_of_quadrant + angle_ignoring_quadrant
+      end
+
+      def x_length
+        @x_length = end_point.x.abs
+      end
+
+      def y_length
+        @y_length ||= end_point.y.abs
+      end
+
+      def angle_to_start_of_quadrant
+        which_side_of_x = end_point.x <=> 0
+        which_side_of_y = end_point.y <=> 0
+
+        case [which_side_of_x, which_side_of_y]
+        when [1,0], [1, 1] # 0-90º
+          0
+        when [0,1], [-1, 1], [-1, 0] # 90-180º
+          DEGREES_90
+        when [-1, -1] # 180-270º
+          DEGREES_180
+        when [0, -1], [1, -1] # 270-360º
+          DEGREES_270
+        end
+      end
+
+      def angle_ignoring_quadrant
+        Math.acos(y_length.to_f/length)
       end
     end
   end
--- spec/draught/line_spec.rb
+++ spec/draught/line_spec.rb
@@ -3,6 +3,71 @@ require 'draught/line'
 
 module Draught
   RSpec.describe Line do
+    def deg_to_rad(degrees)
+      degrees * (Math::PI / 180)
+    end
+
+    describe "building a Line between two Points" do
+      let(:finish) { Point.new(4,4) }
+      subject { Line.build(end_point: finish) }
+
+      it "knows how long it is" do
+        expect(subject.length).to be_within(0.01).of(5.66)
+      end
+
+      it "knows what angle (in radians) it's at" do
+        expect(subject.radians).to be_within(0.0001).of(deg_to_rad(45))
+      end
+
+      it "can output a Path representing itself" do
+        expect(subject.path).to eq(Path.new([Point::ZERO, finish]))
+      end
+
+      context "angles >= 90º" do
+        it "copes with a line of angle 90º" do
+          line = Line.build(end_point: Point.new(0,4))
+
+          expect(line.length).to be_within(0.01).of(4)
+          expect(line.radians).to be_within(0.0001).of(deg_to_rad(90))
+        end
+
+        it "copes with a line of angle < 180º" do
+          line = Line.build(end_point: Point.new(-4,4))
+
+          expect(line.length).to be_within(0.01).of(5.66)
+          expect(line.radians).to be_within(0.0001).of(deg_to_rad(135))
+        end
+
+        it "copes with a line of angle 180º" do
+          line = Line.build(end_point: Point.new(-4,0))
+
+          expect(line.length).to be_within(0.01).of(4)
+          expect(line.radians).to be_within(0.0001).of(deg_to_rad(180))
+        end
+
+        it "copes with a line of angle < 270º" do
+          line = Line.build(end_point: Point.new(-4,-4))
+
+          expect(line.length).to be_within(0.01).of(5.66)
+          expect(line.radians).to be_within(0.0001).of(deg_to_rad(225))
+        end
+
+        it "copes with a line of angle 270º" do
+          line = Line.build(end_point: Point.new(0,-4))
+
+          expect(line.length).to be_within(0.01).of(4)
+          expect(line.radians).to be_within(0.0001).of(deg_to_rad(270))
+        end
+
+        it "copes with a line of angle < 360º" do
+          line = Line.build(end_point: Point.new(4,-4))
+
+          expect(line.length).to be_within(0.01).of(5.66)
+          expect(line.radians).to be_within(0.0001).of(deg_to_rad(315))
+        end
+      end
+    end
+
     describe "generating horizontal lines" do
       specify "a line of width N is a Path with points at (0,0) and (N,0)" do
         expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(10, 0) }
@@ -16,6 +81,94 @@ module Draught
         expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(0, 10) }
 
         expect(Line.vertical(10)).to eq(expected)
+      end
+    end
+
+    describe "generating lines of a given length and angle" do
+      def path(end_point)
+        Path.new([Point::ZERO, end_point])
+      end
+
+      let(:length) { 5.656854 }
+
+      it "copes with a line of angle 0º" do
+        line = Line.build(length: length, radians: 0)
+
+        expect(line.path).to eq(path(Point.new(length,0)))
+      end
+
+      it "copes with a line of angle 45º" do
+        line = Line.build(length: length, radians: deg_to_rad(45))
+
+        expect(line.path).to approximate(path(Point.new(4,4))).within(0.00001)
+      end
+
+      it "copes with a line of angle 90º" do
+        line = Line.build(length: length, radians: deg_to_rad(90))
+
+        expect(line.path).to eq(path(Point.new(0,length)))
+      end
+
+      it "copes with a line of angle < 180º" do
+        line = Line.build(length: length, radians: deg_to_rad(135))
+
+        expect(line.path).to approximate(path(Point.new(-4,4))).within(0.00001)
+      end
+
+      it "copes with a line of angle 180º" do
+        line = Line.build(length: length, radians: deg_to_rad(180))
+
+        expect(line.path).to eq(path(Point.new(-length,0)))
+      end
+
+      it "copes with a line of angle < 270º" do
+        line = Line.build(length: length, radians: deg_to_rad(225))
+
+        expect(line.path).to approximate(path(Point.new(-4,-4))).within(0.00001)
+      end
+
+      it "copes with a line of angle 270º" do
+        line = Line.build(length: length, radians: deg_to_rad(270))
+
+        expect(line.path).to eq(path(Point.new(0,-length)))
+      end
+
+      it "copes with a line of angle < 360º" do
+        line = Line.build(length: length, radians: deg_to_rad(315))
+
+        expect(line.path).to approximate(path(Point.new(4,-4))).within(0.00001)
+      end
+
+      context "ludicrous angles" do
+        it "treats a 360º angle as 0º" do
+          line = Line.build(length: length, radians: deg_to_rad(360))
+
+          expect(line.path).to eq(path(Point.new(length,0)))
+        end
+
+        it "treats a > 360º angle properly" do
+          line = Line.build(length: length, radians: deg_to_rad(495))
+
+          expect(line.path).to approximate(path(Point.new(-4,4))).within(0.00001)
+        end
+
+        it "treats a > 360º right-angle properly" do
+          line = Line.build(length: length, radians: deg_to_rad(450))
+
+          expect(line.path).to eq(path(Point.new(0,length)))
+        end
+
+        it "treats a > 720º angle properly" do
+          line = Line.build(length: length, radians: deg_to_rad(1035))
+
+          expect(line.path).to approximate(path(Point.new(4,-4))).within(0.00001)
+        end
+
+        it "treats a > 720º right-angle properly" do
+          line = Line.build(length: length, radians: deg_to_rad(630))
+
+          expect(line.path).to eq(path(Point.new(0,-length)))
+        end
       end
     end
   end
