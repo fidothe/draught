88569bfe1dbb45561038c58f9fba20fa76940d7f
Matt Patterson <matt@reprocessed.org>
2017-05-25 13:22:42 +0200

    Refactor the Corner-joining code
    Extract the giant method for joining two paths into sensible short
    methods, and extract the code concerned with calculating the join angle,
    tangents and such into another class.
    
    Need to figure out whether to re-write the tests to exercise the public
    interfaces of these new classes directly, and correspondingly reduce the
    amount of tests needed for the join-lots-of-paths code

--- lib/draught/corner.rb
+++ lib/draught/corner.rb
@@ -18,11 +18,15 @@ module Draught
 
     def join
       paths.inject { |incoming, outgoing|
-        Rounded.new(radius: radius, incoming: incoming, outgoing: outgoing).join
+        Rounded.join(radius: radius, incoming: incoming, outgoing: outgoing)
       }
     end
 
     class Rounded
+      def self.join(args)
+        new(args).joined
+      end
+
       attr_reader :radius, :incoming, :outgoing
 
       def initialize(args)
@@ -31,60 +35,131 @@ module Draught
         @outgoing = args.fetch(:outgoing)
       end
 
-      def join
-        incoming_before_final_segment = incoming[0..-2]
-        incoming_line = Line.from_path(incoming[-2,2])
-        outgoing_after_first_segment = outgoing[1..-1]
-        outgoing_line = Line.from_path(outgoing[0..1])
-
-        incoming_corner_line = incoming_line.extend(to: 1)
-        incoming_corner_line = incoming_corner_line.translate(
-          Vector.translation_between(incoming_corner_line.last, Point::ZERO)
-        )
-        outgoing_corner_line = outgoing_line.extend(to: 1)
-        outgoing_corner_line = outgoing_corner_line.translate(
-          Vector.translation_between(outgoing_corner_line.first, Point::ZERO)
-        )
-        corner_top_line = Line.build({
-          start_point: incoming_corner_line.start_point, end_point: outgoing_corner_line.end_point
-        })
-        corner_top_line = corner_top_line.extend(to: corner_top_line.length / 2.0)
-        corner_bisection_line = Line.build({
-          start_point: incoming_corner_line.end_point, end_point: corner_top_line.end_point
-        })
-        half_corner_angle = Math.asin(corner_top_line.length/incoming_corner_line.length)
-        corner_angle = half_corner_angle * 2.0
-
-        distance_to_tangent = radius / Math.tan(corner_angle / 2.0)
-
-        incoming_final_segment = incoming_line.extend(by: -distance_to_tangent)
-        outgoing_first_segment = outgoing_line.extend(by: -distance_to_tangent, at: :start)
-
-        joined_lines = PathBuilder.connect(incoming_line, outgoing_line)
-        zeroed_joined_lines = joined_lines.translate(Vector.translation_to_zero(joined_lines.points.first))
-        aligned_zeroed_lines = zeroed_joined_lines.transform(Transformations.rotate(incoming_line.radians * -1))
-
-        corner_angle_negative = if aligned_zeroed_lines[0].x > aligned_zeroed_lines[1].x
-          aligned_zeroed_lines[0].y < aligned_zeroed_lines[2].y
-        else
-          aligned_zeroed_lines[0].y > aligned_zeroed_lines[2].y
-        end
-        arc_sweep = Math::PI - corner_angle
-        if corner_angle_negative
-          arc_sweep = arc_sweep * -1
-          corner_angle = corner_angle * -1
-        end
-        starting_angle = incoming_corner_line.radians - (Math::PI / 2)
-
-        corner_arc_path = ArcBuilder.radians(angle: arc_sweep, radius: radius, starting_angle: starting_angle).path
-        corner_path = PathBuilder.connect(
+      def joined
+        PathBuilder.connect(
           incoming_before_final_segment,
           incoming_final_segment,
           corner_arc_path,
           outgoing_first_segment,
           outgoing_after_first_segment
         )
-        corner_path
+      end
+
+      def incoming_before_final_segment
+        incoming[0..-2]
+      end
+
+      def incoming_final_segment
+        incoming_line.extend(by: -distance_to_tangent)
+      end
+
+      def incoming_line
+        Line.from_path(incoming[-2,2])
+      end
+
+      def outgoing_first_segment
+        outgoing_line.extend(by: -distance_to_tangent, at: :start)
+      end
+
+      def outgoing_line
+        Line.from_path(outgoing[0..1])
+      end
+
+      def outgoing_after_first_segment
+        outgoing[1..-1]
+      end
+
+      def distance_to_tangent
+        @distance_to_tangent ||= join_angles.tangent_distance(radius)
+      end
+
+      def join_angles
+        @join_angles ||= JoinAngles.new(incoming_line, outgoing_line)
+      end
+
+      def corner_arc_path
+        ArcBuilder.radians(angle: join_angles.arc_sweep, radius: radius, starting_angle: starting_angle).path
+      end
+
+      def starting_angle
+        incoming_line.radians - (Math::PI / 2)
+      end
+    end
+
+    class JoinAngles
+      attr_reader :incoming_line, :outgoing_line
+
+      def initialize(incoming_line, outgoing_line)
+        @incoming_line = incoming_line
+        @outgoing_line = outgoing_line
+      end
+
+      def arc_sweep
+        anticlockwise? ? abs_arc_sweep : abs_arc_sweep * -1
+      end
+
+      def tangent_distance(radius)
+        radius / Math.tan(abs_corner_angle / 2.0)
+      end
+
+      private
+
+      def abs_corner_angle
+        @abs_corner_angle ||= begin
+          half_corner_angle = Math.asin(corner_top_line.length/incoming_corner_line.length)
+          half_corner_angle * 2.0
+        end
+      end
+
+      def corner_top_line
+        @corner_top_line ||= begin
+          corner_top_line = Line.build({
+            start_point: incoming_corner_line.start_point, end_point: outgoing_corner_line.end_point
+          })
+          corner_top_line.extend(to: corner_top_line.length / 2.0)
+        end
+      end
+
+      def incoming_corner_line
+        @incoming_corner_line ||= begin
+          incoming_corner_line = incoming_line.extend(to: 1)
+          incoming_corner_line.translate(
+            Vector.translation_to_zero(incoming_corner_line.last)
+          )
+        end
+      end
+
+      def outgoing_corner_line
+        @outgoing_corner_line ||= begin
+          outgoing_corner_line = outgoing_line.extend(to: 1)
+          outgoing_corner_line.translate(
+            Vector.translation_to_zero(outgoing_corner_line.first)
+          )
+        end
+      end
+
+      def abs_arc_sweep
+        @abs_arc_sweep ||= Math::PI - abs_corner_angle
+      end
+
+      def anticlockwise?
+        !clockwise?
+      end
+
+      def clockwise?
+        if aligned_zeroed_lines[0].x > aligned_zeroed_lines[1].x
+          aligned_zeroed_lines[0].y < aligned_zeroed_lines[2].y
+        else
+          aligned_zeroed_lines[0].y > aligned_zeroed_lines[2].y
+        end
+      end
+
+      def aligned_zeroed_lines
+        @aligned_zeroed_lines ||= begin
+          joined_lines = PathBuilder.connect(incoming_line, outgoing_line)
+          zeroed_joined_lines = joined_lines.translate(Vector.translation_to_zero(joined_lines.first))
+          zeroed_joined_lines.transform(Transformations.rotate(incoming_line.radians * -1))
+        end
       end
     end
   end
