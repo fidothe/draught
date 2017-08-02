fidothe/box_dividers

Extract pathlike qualities to an interface
Now Lines and Paths are both path-like. This is to allow keeping Lines
around as Lines for as long as possible - in the corner-rounding work
being done at the moment the separate length and angle properties of
Lines are massively helpful

lib/box_dividers/pathlike.rb
@@ -1,0 +1,53 @@
+module BoxDividers
+  module Pathlike
+    def points
+      raise NotImplementedError, "Pathlike objects must return an array of their points"
+    end
+
+    def translate(vector)
+      raise NotImplementedError, "Pathlike objects must implement translation by Vector"
+    end
+
+    def transform(transformation)
+      raise NotImplementedError, "Pathlike objects must implement transformation by Affine transform or point-taking lambda"
+    end
+
+    def [](index_start_or_range, length = nil)
+      raise NotImplementedError, "Pathlike objects must implement [] access on their points, returning a new instance"
+    end
+
+    def number_of_points
+      points.length
+    end
+
+    def first
+      points.first
+    end
+
+    def last
+      points.last
+    end
+
+    def empty?
+      points.empty?
+    end
+
+    def ==(other)
+      return false if number_of_points != other.number_of_points
+      points.zip(other.points).all? { |a, b| a == b }
+    end
+
+    def approximates?(other, delta)
+      return false if number_of_points != other.number_of_points
+      points.zip(other.points).all? { |a, b| a.approximates?(b, delta) }
+    end
+
+    def paths
+      [self]
+    end
+
+    def containers
+      []
+    end
+  end
+end

spec/box_dividers/line_spec.rb
@@ -1,4 +1,5 @@
 require 'box_dividers/path_builder'
+require 'box_dividers/pathlike_examples'
 require 'box_dividers/line'
 
 module BoxDividers
@@ -17,10 +18,6 @@ module BoxDividers
 
       it "knows what angle (in radians) it's at" do
         expect(subject.radians).to be_within(0.0001).of(deg_to_rad(45))
-      end
-
-      it "can output a Path representing itself" do
-        expect(subject.path).to eq(Path.new([Point::ZERO, finish]))
       end
 
       context "angles >= 90º" do
@@ -69,18 +66,18 @@ module BoxDividers
     end
 
     describe "generating horizontal lines" do
-      specify "a line of width N is a Path with points at (0,0) and (N,0)" do
+      specify "a line of width N is like a Path with points at (0,0) and (N,0)" do
         expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(10, 0) }
 
-        expect(Line.horizontal(10).path).to eq(expected)
+        expect(Line.horizontal(10)).to eq(expected)
       end
     end
 
     describe "generating vertical lines" do
-      specify "a line of height N is a Path with points at (0,0) and (0,N)" do
+      specify "a line of height N is like a Path with points at (0,0) and (0,N)" do
         expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(0, 10) }
 
-        expect(Line.vertical(10).path).to eq(expected)
+        expect(Line.vertical(10)).to eq(expected)
       end
     end
 
@@ -221,6 +218,12 @@ module BoxDividers
           expect(subject.lengthen(2, :from_start).path).to approximate(expected).within(0.00001)
         end
       end
+    end
+
+    it_should_behave_like "a pathlike thing" do
+      let(:end_point) { Point.new(4,4) }
+      let(:points) { [Point::ZERO, end_point] }
+      subject { Line.build(end_point: end_point) }
     end
   end
 end

spec/box_dividers/path_spec.rb
@@ -1,4 +1,5 @@
 require 'box_dividers/boxlike_examples'
+require 'box_dividers/pathlike_examples'
 require 'box_dividers/point'
 require 'box_dividers/vector'
 require 'box_dividers/transformations'
@@ -7,9 +8,10 @@ require 'box_dividers/path'
 module BoxDividers
   RSpec.describe Path do
     let(:point) { Point.new(1,1) }
+    let(:other_point) { Point.new(2,2) }
 
     it "contains no points by default" do
-      expect(subject.empty?).to be(true)
+      expect(Path.new.empty?).to be(true)
     end
 
     it "can be initialized with an array of Points" do
@@ -18,15 +20,13 @@ module BoxDividers
       expect(path.empty?).to be(false)
     end
 
-    it "can return an array of its points" do
-      path = Path.new([point])
-
-      expect(path.points).to eq([point])
+    it_should_behave_like "a pathlike thing" do
+      subject { Path.new([point, other_point]) }
+      let(:points) { [point, other_point] }
     end
 
     describe "adding points to the path" do
       subject { Path.new([point]) }
-      let(:other_point) { Point.new(2,2) }
 
       context "appending" do
         specify "appending a point to the path returns a new path" do
@@ -83,23 +83,13 @@ module BoxDividers
       subject { Path.new([p1, p2, p3]) }
 
       context "[] access" do
-        it "can retrieve a single item from itself" do
-          expect(subject[1]).to eq(p2)
-        end
-
-        it "can retrieve a slice via range" do
+        it "can also retrieve a slice via range" do
           expect(subject[1..2]).to eq(Path.new([p2, p3]))
         end
 
-        it "can retrieve a slice via (n,n) args" do
+        it "can also retrieve a slice via (n,n) args" do
           expect(subject[0,2]).to eq(Path.new([p1, p2]))
         end
-      end
-
-      context "first and last readers" do
-        specify { expect(subject.first).to eq(p1) }
-
-        specify { expect(subject.last).to eq(p3) }
       end
     end
 
@@ -122,21 +112,6 @@ module BoxDividers
 
           expect(subject == path).to be(false)
         end
-
-        it "compares two Paths unequal if one or more of their Points are not equal" do
-          path = Path.new([p1, p3])
-
-          expect(subject == path).to be(false)
-        end
-
-        it "compares equal to a (0,0) translation of itself" do
-          expect(subject.translate(BoxDividers::Vector.new(0,0))).to eq(subject)
-        end
-      end
-
-      it "compares two Paths approximately equal if all their Points are approximately equal" do
-        approx_path = subject.translate(Vector.new(0.000001, 0.000001))
-        expect(subject.approximates?(approx_path, 0.00001)).to be(true)
       end
     end
 
@@ -156,37 +131,6 @@ module BoxDividers
           expect(subject.upper_right).to eq(Point.new(0,0))
         end
       end
-    end
-
-    describe "translation and transformation" do
-      let(:p1) { Point.new(1,1) }
-      let(:p2) { Point.new(1,2) }
-      let(:vector) { Vector.new(2,1) }
-
-      subject { Path.new([p1, p2]) }
-
-      specify "translating a Path using a Point produces a new Path with appropriately translated Points" do
-        expected = Path.new([Point.new(3,2), Point.new(3,3)])
-
-        expect(subject.translate(vector)).to eq(expected)
-      end
-
-      specify "transforming a Path generates a new Path by applying the transformation to every Point in the Path" do
-        transformation = BoxDividers::Transformations::Affine.new(
-          Matrix[[2,0,0],[0,2,0],[0,0,1]]
-        )
-        expected = Path.new([Point.new(2,2), Point.new(2,4)])
-
-        expect(subject.transform(transformation)).to eq(expected)
-      end
-    end
-
-    it "returns an Array of itself for #paths" do
-      expect(subject.paths).to eq([subject])
-    end
-
-    it "returns an empty Array for #containers" do
-      expect(subject.containers).to eq([])
     end
   end
 end

spec/box_dividers/pathlike_examples.rb
@@ -1,0 +1,64 @@
+RSpec.shared_examples "a pathlike thing" do
+  it "can return an array of its points" do
+    expect(subject.points).to eq(points)
+  end
+
+  it "reports how many points it has" do
+    expect(subject.number_of_points).to eq(points.length)
+  end
+
+  describe "being enumerable enough" do
+    it "provides [] access to its points" do
+      expect(subject[0]).to eq(subject.points.first)
+    end
+
+    context "provides first and last readers" do
+      specify { expect(subject.first).to eq(points.first) }
+
+      specify { expect(subject.last).to eq(points.last) }
+    end
+  end
+
+  describe "comparison" do
+    it "compares equal to a (0,0) translation of itself" do
+      expect(subject.translate(BoxDividers::Vector::NULL)).to eq(subject)
+    end
+
+    it "does not compare equal to a (1,1) translation of itself" do
+      expect(subject.translate(BoxDividers::Vector.new(1,1))).not_to eq(subject)
+    end
+
+    it "compares approximately equal to a slightly nudged translation of itself" do
+      approx_pathlike = subject.translate(BoxDividers::Vector.new(0.000001, 0.000001))
+      expect(subject.approximates?(approx_pathlike, 0.00001)).to be(true)
+    end
+  end
+
+  describe "translation and transformation" do
+    specify "translating a Path using a Point produces a new Path with appropriately translated Points" do
+      translation = BoxDividers::Vector.new(2,1)
+      expected = points.map { |p| p.translate(translation) }
+
+      expect(subject.translate(translation).points).to eq(expected)
+    end
+
+    specify "transforming a Path generates a new Path by applying the transformation to every Point in the Path" do
+      transformation = BoxDividers::Transformations::Affine.new(
+        Matrix[[2,0,0],[0,2,0],[0,0,1]]
+      )
+      expected = points.map { |p| p.transform(transformation) }
+
+      expect(subject.transform(transformation).points).to eq(expected)
+    end
+  end
+
+  context "renderer methods" do
+    it "returns an Array of itself for #paths" do
+      expect(subject.paths).to eq([subject])
+    end
+
+    it "returns an empty Array for #containers" do
+      expect(subject.containers).to eq([])
+    end
+  end
+end

fidothe/draught

lib/draught/pathlike.rb
@@ -1,0 +1,53 @@
+module Draught
+  module Pathlike
+    def points
+      raise NotImplementedError, "Pathlike objects must return an array of their points"
+    end
+
+    def translate(vector)
+      raise NotImplementedError, "Pathlike objects must implement translation by Vector"
+    end
+
+    def transform(transformation)
+      raise NotImplementedError, "Pathlike objects must implement transformation by Affine transform or point-taking lambda"
+    end
+
+    def [](index_start_or_range, length = nil)
+      raise NotImplementedError, "Pathlike objects must implement [] access on their points, returning a new instance"
+    end
+
+    def number_of_points
+      points.length
+    end
+
+    def first
+      points.first
+    end
+
+    def last
+      points.last
+    end
+
+    def empty?
+      points.empty?
+    end
+
+    def ==(other)
+      return false if number_of_points != other.number_of_points
+      points.zip(other.points).all? { |a, b| a == b }
+    end
+
+    def approximates?(other, delta)
+      return false if number_of_points != other.number_of_points
+      points.zip(other.points).all? { |a, b| a.approximates?(b, delta) }
+    end
+
+    def paths
+      [self]
+    end
+
+    def containers
+      []
+    end
+  end
+end

spec/draught/line_spec.rb
@@ -1,4 +1,5 @@
 require 'draught/path_builder'
+require 'draught/pathlike_examples'
 require 'draught/line'
 
 module Draught
@@ -17,10 +18,6 @@ module Draught
 
       it "knows what angle (in radians) it's at" do
         expect(subject.radians).to be_within(0.0001).of(deg_to_rad(45))
-      end
-
-      it "can output a Path representing itself" do
-        expect(subject.path).to eq(Path.new([Point::ZERO, finish]))
       end
 
       context "angles >= 90º" do
@@ -69,18 +66,18 @@ module Draught
     end
 
     describe "generating horizontal lines" do
-      specify "a line of width N is a Path with points at (0,0) and (N,0)" do
+      specify "a line of width N is like a Path with points at (0,0) and (N,0)" do
         expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(10, 0) }
 
-        expect(Line.horizontal(10).path).to eq(expected)
+        expect(Line.horizontal(10)).to eq(expected)
       end
     end
 
     describe "generating vertical lines" do
-      specify "a line of height N is a Path with points at (0,0) and (0,N)" do
+      specify "a line of height N is like a Path with points at (0,0) and (0,N)" do
         expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(0, 10) }
 
-        expect(Line.vertical(10).path).to eq(expected)
+        expect(Line.vertical(10)).to eq(expected)
       end
     end
 
@@ -221,6 +218,12 @@ module Draught
           expect(subject.lengthen(2, :from_start).path).to approximate(expected).within(0.00001)
         end
       end
+    end
+
+    it_should_behave_like "a pathlike thing" do
+      let(:end_point) { Point.new(4,4) }
+      let(:points) { [Point::ZERO, end_point] }
+      subject { Line.build(end_point: end_point) }
     end
   end
 end

spec/draught/path_spec.rb
@@ -1,4 +1,5 @@
 require 'draught/boxlike_examples'
+require 'draught/pathlike_examples'
 require 'draught/point'
 require 'draught/vector'
 require 'draught/transformations'
@@ -7,9 +8,10 @@ require 'draught/path'
 module Draught
   RSpec.describe Path do
     let(:point) { Point.new(1,1) }
+    let(:other_point) { Point.new(2,2) }
 
     it "contains no points by default" do
-      expect(subject.empty?).to be(true)
+      expect(Path.new.empty?).to be(true)
     end
 
     it "can be initialized with an array of Points" do
@@ -18,15 +20,13 @@ module Draught
       expect(path.empty?).to be(false)
     end
 
-    it "can return an array of its points" do
-      path = Path.new([point])
-
-      expect(path.points).to eq([point])
+    it_should_behave_like "a pathlike thing" do
+      subject { Path.new([point, other_point]) }
+      let(:points) { [point, other_point] }
     end
 
     describe "adding points to the path" do
       subject { Path.new([point]) }
-      let(:other_point) { Point.new(2,2) }
 
       context "appending" do
         specify "appending a point to the path returns a new path" do
@@ -83,23 +83,13 @@ module Draught
       subject { Path.new([p1, p2, p3]) }
 
       context "[] access" do
-        it "can retrieve a single item from itself" do
-          expect(subject[1]).to eq(p2)
-        end
-
-        it "can retrieve a slice via range" do
+        it "can also retrieve a slice via range" do
           expect(subject[1..2]).to eq(Path.new([p2, p3]))
         end
 
-        it "can retrieve a slice via (n,n) args" do
+        it "can also retrieve a slice via (n,n) args" do
           expect(subject[0,2]).to eq(Path.new([p1, p2]))
         end
-      end
-
-      context "first and last readers" do
-        specify { expect(subject.first).to eq(p1) }
-
-        specify { expect(subject.last).to eq(p3) }
       end
     end
 
@@ -122,21 +112,6 @@ module Draught
 
           expect(subject == path).to be(false)
         end
-
-        it "compares two Paths unequal if one or more of their Points are not equal" do
-          path = Path.new([p1, p3])
-
-          expect(subject == path).to be(false)
-        end
-
-        it "compares equal to a (0,0) translation of itself" do
-          expect(subject.translate(Draught::Vector.new(0,0))).to eq(subject)
-        end
-      end
-
-      it "compares two Paths approximately equal if all their Points are approximately equal" do
-        approx_path = subject.translate(Vector.new(0.000001, 0.000001))
-        expect(subject.approximates?(approx_path, 0.00001)).to be(true)
       end
     end
 
@@ -156,37 +131,6 @@ module Draught
           expect(subject.upper_right).to eq(Point.new(0,0))
         end
       end
-    end
-
-    describe "translation and transformation" do
-      let(:p1) { Point.new(1,1) }
-      let(:p2) { Point.new(1,2) }
-      let(:vector) { Vector.new(2,1) }
-
-      subject { Path.new([p1, p2]) }
-
-      specify "translating a Path using a Point produces a new Path with appropriately translated Points" do
-        expected = Path.new([Point.new(3,2), Point.new(3,3)])
-
-        expect(subject.translate(vector)).to eq(expected)
-      end
-
-      specify "transforming a Path generates a new Path by applying the transformation to every Point in the Path" do
-        transformation = Draught::Transformations::Affine.new(
-          Matrix[[2,0,0],[0,2,0],[0,0,1]]
-        )
-        expected = Path.new([Point.new(2,2), Point.new(2,4)])
-
-        expect(subject.transform(transformation)).to eq(expected)
-      end
-    end
-
-    it "returns an Array of itself for #paths" do
-      expect(subject.paths).to eq([subject])
-    end
-
-    it "returns an empty Array for #containers" do
-      expect(subject.containers).to eq([])
     end
   end
 end

spec/draught/pathlike_examples.rb
@@ -1,0 +1,64 @@
+RSpec.shared_examples "a pathlike thing" do
+  it "can return an array of its points" do
+    expect(subject.points).to eq(points)
+  end
+
+  it "reports how many points it has" do
+    expect(subject.number_of_points).to eq(points.length)
+  end
+
+  describe "being enumerable enough" do
+    it "provides [] access to its points" do
+      expect(subject[0]).to eq(subject.points.first)
+    end
+
+    context "provides first and last readers" do
+      specify { expect(subject.first).to eq(points.first) }
+
+      specify { expect(subject.last).to eq(points.last) }
+    end
+  end
+
+  describe "comparison" do
+    it "compares equal to a (0,0) translation of itself" do
+      expect(subject.translate(Draught::Vector::NULL)).to eq(subject)
+    end
+
+    it "does not compare equal to a (1,1) translation of itself" do
+      expect(subject.translate(Draught::Vector.new(1,1))).not_to eq(subject)
+    end
+
+    it "compares approximately equal to a slightly nudged translation of itself" do
+      approx_pathlike = subject.translate(Draught::Vector.new(0.000001, 0.000001))
+      expect(subject.approximates?(approx_pathlike, 0.00001)).to be(true)
+    end
+  end
+
+  describe "translation and transformation" do
+    specify "translating a Path using a Point produces a new Path with appropriately translated Points" do
+      translation = Draught::Vector.new(2,1)
+      expected = points.map { |p| p.translate(translation) }
+
+      expect(subject.translate(translation).points).to eq(expected)
+    end
+
+    specify "transforming a Path generates a new Path by applying the transformation to every Point in the Path" do
+      transformation = Draught::Transformations::Affine.new(
+        Matrix[[2,0,0],[0,2,0],[0,0,1]]
+      )
+      expected = points.map { |p| p.transform(transformation) }
+
+      expect(subject.transform(transformation).points).to eq(expected)
+    end
+  end
+
+  context "renderer methods" do
+    it "returns an Array of itself for #paths" do
+      expect(subject.paths).to eq([subject])
+    end
+
+    it "returns an empty Array for #containers" do
+      expect(subject.containers).to eq([])
+    end
+  end
+end