fidothe/box_dividers

Move to explicit transformation classes
Introduce Affine and Proclike transformation classes to cover the main
cases of matrix-based easily-composable transforms and
transforms-using-procs. Add a Composer class that can compose both kinds
of transforms together, and can coalesce sequences of Affine transforms
down to a single operation.

Allow points to be simply constructed from a 1-column matrix

lib/box_dividers/transformations/affine.rb
@@ -1,0 +1,33 @@
+require_relative 'common'
+require_relative '../point'
+
+module BoxDividers
+  module Transformations
+    class Affine
+      include Common
+
+      attr_reader :transformation_matrix
+
+      def initialize(transformation_matrix)
+        @transformation_matrix = transformation_matrix
+      end
+
+      def call(point)
+        Point.from_matrix(transformation_matrix * point.to_matrix)
+      end
+
+      def affine?
+        true
+      end
+
+      def ==(other)
+        other.respond_to?(:transformation_matrix) && other.transformation_matrix == transformation_matrix
+      end
+
+      def coalesce(other)
+        raise TypeError, "other must be a matrix-based Affine transform" unless other.affine?
+        self.class.new(other.transformation_matrix * self.transformation_matrix)
+      end
+    end
+  end
+end

lib/box_dividers/transformations/common.rb
@@ -1,0 +1,23 @@
+require_relative './composer'
+
+module BoxDividers
+  module Transformations
+    module Common
+      def call(point)
+        raise NotImplementedError, "Classes including Transformations::Common must implement #call, taking a Point and returning a new, transformed, Point"
+      end
+
+      def affine?
+        raise NotImplementedError, "Classes including Transformations::Common must implement #affine?"
+      end
+
+      def flattened_transforms
+        [self]
+      end
+
+      def compose(other)
+        Composer.coalesced(other, self)
+      end
+    end
+  end
+end

lib/box_dividers/transformations/composer.rb
@@ -1,0 +1,57 @@
+module BoxDividers
+  module Transformations
+    class Composer
+      def self.coalesced(*transforms)
+        new(*transforms).coalesce
+      end
+
+      attr_reader :transforms
+
+      def initialize(*transforms)
+        @transforms = transforms
+      end
+
+      def call(point)
+        transforms.inject(point) { |point, transform| transform.call(point) }
+      end
+
+      def ==(other)
+        other.respond_to?(:coalesce) && other.transforms == transforms
+      end
+
+      def coalesce
+        self.class.new(*coalesced_transforms)
+      end
+
+      def coalesced_transforms
+        return [] if transforms.empty?
+        start_transforms = flattened_transforms
+        finished = start_transforms.shift(1)
+        return finished if start_transforms.empty?
+
+        start_transforms.each do |next_transform|
+          coalesce_pair(finished.pop, next_transform).each do |coalesced_transform|
+            finished << coalesced_transform
+          end
+        end
+        finished
+      end
+
+      def flattened_transforms
+        transforms.flat_map { |transform|
+          transform.flattened_transforms
+        }
+      end
+
+      private
+
+      def coalesce_pair(first, second)
+        begin
+          [first.coalesce(second)]
+        rescue TypeError
+          [first, second]
+        end
+      end
+    end
+  end
+end

spec/box_dividers/transformations/affine_spec.rb
@@ -1,0 +1,59 @@
+require 'box_dividers/transformations/affine'
+require 'box_dividers/transformations/proclike'
+require 'box_dividers/transformations/shared_examples'
+require 'box_dividers/point'
+
+module BoxDividers::Transformations
+  RSpec.describe Affine do
+    let(:transformation_matrix) {
+      Matrix[[-1, 0, 0],[0, -1, 0],[0,0,1]]
+    }
+    let(:input_point) { BoxDividers::Point.new(1,2) }
+    let(:result_point) { BoxDividers::Point.new(-1,-2) }
+
+    subject { Affine.new(transformation_matrix) }
+
+    it_should_behave_like "a well-behaved transformation class"
+
+    it "claims to be affine" do
+      expect(Affine.new(transformation_matrix).affine?).to be true
+    end
+
+    context "coalescing two Affine transforms together into a new transform" do
+      let(:t1) { Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]]) }
+      let(:t2) { Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]]) }
+
+      specify "produces a new Affine transform by matrix multiplication" do
+        expect(t2.coalesce(t1).call(input_point)).to eq(BoxDividers::Point.new(-1,-2))
+      end
+
+      specify "the matrix of the new transform is the product of the inputs" do
+        expected_matrix = Matrix[[-1, 0, 0],[0, -1, 0],[0, 0, 1]]
+
+        coalesced = t2.coalesce(t1)
+
+        expect(coalesced.transformation_matrix).to eq(expected_matrix)
+      end
+
+      specify "attempting to coalesce an Affine transform with another kind raises TypeError" do
+        other = Proclike.new(->(p) { p })
+
+        expect { subject.coalesce(other) }.to raise_error(TypeError)
+      end
+    end
+
+    specify "two Affine transforms can be composed into a single one" do
+      t1 = Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]])
+      t2 = Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]])
+
+      expect(t2.compose(t1).call(input_point)).to eq(BoxDividers::Point.new(-1,-2))
+    end
+
+    specify "an Affine transform can be composed with a Proclike" do
+      t1 = Proclike.new(->(p) { [p.x + 2, p.y + 2] })
+      t2 = Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]])
+
+      expect(t2.compose(t1).call(input_point)).to eq(BoxDividers::Point.new(3,-4))
+    end
+  end
+end

spec/box_dividers/transformations/composer_spec.rb
@@ -1,0 +1,98 @@
+require 'box_dividers/transformations/composer'
+require 'box_dividers/transformations/affine'
+require 'box_dividers/transformations/proclike'
+require 'box_dividers/point'
+
+module BoxDividers::Transformations
+  RSpec.describe Composer do
+    let(:t1) { Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]]) }
+    let(:t2) { Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]]) }
+
+    it "is initialized with a pair of transforms" do
+      expect(Composer.new(t2, t1)).to be_a(Composer)
+    end
+
+    it "compares equality based on its transforms" do
+      expect(Composer.new(t2, t1)).to eq(Composer.new(t2, t1))
+    end
+
+    context "being #call()ed" do
+      subject { Composer.new(t1, t2) }
+
+      it "returns the Point that results from passing its argument through all its transforms in the correct order" do
+        input = BoxDividers::Point.new(1,2)
+        expected = BoxDividers::Point.new(-1,-2)
+      end
+    end
+
+    context "optimisations" do
+      let(:t3) { Affine.new(Matrix[[2, 0, 0],[0, 2, 0],[0, 0, 1]]) }
+      let(:composed) { Composer.new(t1, t2) }
+      let(:input) { BoxDividers::Point.new(1,2) }
+      let(:expected) { BoxDividers::Point.new(-2,-4) }
+
+      it "can flatten the transforms from composed Composer objects into a single Composer array" do
+        composer = Composer.new(composed, t3)
+
+        expect(composer.flattened_transforms).to eq([t1, t2, t3])
+      end
+
+      context "coalescing transforms" do
+        let(:pt) { Proclike.new(->(p) { p }) }
+        let(:t1_t2_coalesced) { Affine.new(Matrix[[-1, 0, 0],[0, -1, 0],[0, 0, 1]]) }
+
+        context "coalescing the transforms" do
+          it "can coalesce a sequence of Affine transforms into a single transform" do
+            expect(composed.coalesced_transforms).to eq([t1_t2_coalesced])
+          end
+
+          it "copes when a non-Affine transform begins the sequence" do
+            composer = Composer.new(pt, t1, t2)
+
+            expect(composer.coalesced_transforms).to eq([pt, t1_t2_coalesced])
+          end
+
+          it "copes with an empty transforms list" do
+            composer = Composer.new()
+
+            expect(composer.coalesced_transforms).to eq([])
+          end
+
+          it "copes with a single-item transform list" do
+            composer = Composer.new(pt)
+
+            expect(composer.coalesced_transforms).to eq([pt])
+          end
+
+          it "copes when a non-Affine transform ends the sequence" do
+            composer = Composer.new(t1, t2, pt)
+
+            expect(composer.coalesced_transforms).to eq([t1_t2_coalesced, pt])
+          end
+
+          it "can coalesce either side of a non-Affine transform" do
+            composer = Composer.new(t1, t2, pt, t1, t2)
+
+            expect(composer.coalesced_transforms).to eq([t1_t2_coalesced, pt, t1_t2_coalesced])
+          end
+
+          it "copes with multiple proclike transforms" do
+            composer = Composer.new(pt, pt)
+
+            expect(composer.coalesced_transforms).to eq([pt, pt])
+          end
+        end
+
+        it "can return a coalesced version of itself" do
+          coalesced = composed.coalesce
+
+          expect(coalesced.transforms).to eq([t1_t2_coalesced])
+        end
+
+        it "offers a convenience creator that returns a coalesced composed version of its passed transforms" do
+          expect(Composer.coalesced(t1, t2)).to eq(Composer.new(t1_t2_coalesced))
+        end
+      end
+    end
+  end
+end

spec/box_dividers/transformations/proclike_spec.rb
@@ -1,0 +1,31 @@
+require 'box_dividers/transformations/proclike'
+require 'box_dividers/transformations/shared_examples'
+require 'box_dividers/point'
+
+module BoxDividers::Transformations
+  RSpec.describe Proclike do
+    let(:array_return_transformer) { ->(p) { [2 * p.x, 2 * p.y] } }
+    let(:point_return_transformer) { ->(p) { BoxDividers::Point.new(p.x + 10, p.y + 10) } }
+    let(:input_point) { BoxDividers::Point.new(1,2) }
+    let(:result_point) { BoxDividers::Point.new(11,12) }
+
+    subject { Proclike.new(point_return_transformer) }
+
+    it_should_behave_like "a well-behaved transformation class"
+
+    describe "running the transformation with #call() and passing in a Point" do
+      it "returns the result as a Point if the block returns an [x,y] tuple" do
+        expect(Proclike.new(array_return_transformer).call(input_point)).
+          to eq(BoxDividers::Point.new(2,4))
+      end
+    end
+
+    specify "the transform is not Affine" do
+      expect(Proclike.new(array_return_transformer).affine?).to be false
+    end
+
+    specify "Proclike transforms cannot be coalesced, so raise TypeError if asked to" do
+        expect { subject.coalesce(subject) }.to raise_error(TypeError)
+    end
+  end
+end

spec/box_dividers/transformations/shared_examples.rb
@@ -1,0 +1,28 @@
+require 'box_dividers/transformations/composer'
+
+RSpec.shared_examples "a well-behaved transformation class" do
+  it "responds to #call(), taking a single Point argument and returning the Point result of applying the transform" do
+    expect(subject.call(input_point)).
+      to eq(result_point)
+  end
+
+  it "compares equal to a dup of itself" do
+    expect(subject.dup).to eq(subject)
+  end
+
+  context "being aware of affine-ness" do
+    it "responds to #affine?" do
+      expect(subject).to respond_to(:affine?)
+    end
+  end
+
+  context "composition" do
+    it "returns a correctly instantiated composed transform in response to #compose" do
+      expect(subject.compose(subject)).to eq(BoxDividers::Transformations::Composer.coalesced(subject, subject))
+    end
+
+    it "returns an array consisting of itself in response to #flattened_transforms" do
+      expect(subject.flattened_transforms).to eq([subject])
+    end
+  end
+end

fidothe/draught

lib/draught/transformations/affine.rb
@@ -1,0 +1,33 @@
+require_relative 'common'
+require_relative '../point'
+
+module Draught
+  module Transformations
+    class Affine
+      include Common
+
+      attr_reader :transformation_matrix
+
+      def initialize(transformation_matrix)
+        @transformation_matrix = transformation_matrix
+      end
+
+      def call(point)
+        Point.from_matrix(transformation_matrix * point.to_matrix)
+      end
+
+      def affine?
+        true
+      end
+
+      def ==(other)
+        other.respond_to?(:transformation_matrix) && other.transformation_matrix == transformation_matrix
+      end
+
+      def coalesce(other)
+        raise TypeError, "other must be a matrix-based Affine transform" unless other.affine?
+        self.class.new(other.transformation_matrix * self.transformation_matrix)
+      end
+    end
+  end
+end

lib/draught/transformations/common.rb
@@ -1,0 +1,23 @@
+require_relative './composer'
+
+module Draught
+  module Transformations
+    module Common
+      def call(point)
+        raise NotImplementedError, "Classes including Transformations::Common must implement #call, taking a Point and returning a new, transformed, Point"
+      end
+
+      def affine?
+        raise NotImplementedError, "Classes including Transformations::Common must implement #affine?"
+      end
+
+      def flattened_transforms
+        [self]
+      end
+
+      def compose(other)
+        Composer.coalesced(other, self)
+      end
+    end
+  end
+end

lib/draught/transformations/composer.rb
@@ -1,0 +1,57 @@
+module Draught
+  module Transformations
+    class Composer
+      def self.coalesced(*transforms)
+        new(*transforms).coalesce
+      end
+
+      attr_reader :transforms
+
+      def initialize(*transforms)
+        @transforms = transforms
+      end
+
+      def call(point)
+        transforms.inject(point) { |point, transform| transform.call(point) }
+      end
+
+      def ==(other)
+        other.respond_to?(:coalesce) && other.transforms == transforms
+      end
+
+      def coalesce
+        self.class.new(*coalesced_transforms)
+      end
+
+      def coalesced_transforms
+        return [] if transforms.empty?
+        start_transforms = flattened_transforms
+        finished = start_transforms.shift(1)
+        return finished if start_transforms.empty?
+
+        start_transforms.each do |next_transform|
+          coalesce_pair(finished.pop, next_transform).each do |coalesced_transform|
+            finished << coalesced_transform
+          end
+        end
+        finished
+      end
+
+      def flattened_transforms
+        transforms.flat_map { |transform|
+          transform.flattened_transforms
+        }
+      end
+
+      private
+
+      def coalesce_pair(first, second)
+        begin
+          [first.coalesce(second)]
+        rescue TypeError
+          [first, second]
+        end
+      end
+    end
+  end
+end

spec/draught/transformations/affine_spec.rb
@@ -1,0 +1,59 @@
+require 'draught/transformations/affine'
+require 'draught/transformations/proclike'
+require 'draught/transformations/shared_examples'
+require 'draught/point'
+
+module Draught::Transformations
+  RSpec.describe Affine do
+    let(:transformation_matrix) {
+      Matrix[[-1, 0, 0],[0, -1, 0],[0,0,1]]
+    }
+    let(:input_point) { Draught::Point.new(1,2) }
+    let(:result_point) { Draught::Point.new(-1,-2) }
+
+    subject { Affine.new(transformation_matrix) }
+
+    it_should_behave_like "a well-behaved transformation class"
+
+    it "claims to be affine" do
+      expect(Affine.new(transformation_matrix).affine?).to be true
+    end
+
+    context "coalescing two Affine transforms together into a new transform" do
+      let(:t1) { Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]]) }
+      let(:t2) { Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]]) }
+
+      specify "produces a new Affine transform by matrix multiplication" do
+        expect(t2.coalesce(t1).call(input_point)).to eq(Draught::Point.new(-1,-2))
+      end
+
+      specify "the matrix of the new transform is the product of the inputs" do
+        expected_matrix = Matrix[[-1, 0, 0],[0, -1, 0],[0, 0, 1]]
+
+        coalesced = t2.coalesce(t1)
+
+        expect(coalesced.transformation_matrix).to eq(expected_matrix)
+      end
+
+      specify "attempting to coalesce an Affine transform with another kind raises TypeError" do
+        other = Proclike.new(->(p) { p })
+
+        expect { subject.coalesce(other) }.to raise_error(TypeError)
+      end
+    end
+
+    specify "two Affine transforms can be composed into a single one" do
+      t1 = Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]])
+      t2 = Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]])
+
+      expect(t2.compose(t1).call(input_point)).to eq(Draught::Point.new(-1,-2))
+    end
+
+    specify "an Affine transform can be composed with a Proclike" do
+      t1 = Proclike.new(->(p) { [p.x + 2, p.y + 2] })
+      t2 = Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]])
+
+      expect(t2.compose(t1).call(input_point)).to eq(Draught::Point.new(3,-4))
+    end
+  end
+end

spec/draught/transformations/composer_spec.rb
@@ -1,0 +1,98 @@
+require 'draught/transformations/composer'
+require 'draught/transformations/affine'
+require 'draught/transformations/proclike'
+require 'draught/point'
+
+module Draught::Transformations
+  RSpec.describe Composer do
+    let(:t1) { Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]]) }
+    let(:t2) { Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]]) }
+
+    it "is initialized with a pair of transforms" do
+      expect(Composer.new(t2, t1)).to be_a(Composer)
+    end
+
+    it "compares equality based on its transforms" do
+      expect(Composer.new(t2, t1)).to eq(Composer.new(t2, t1))
+    end
+
+    context "being #call()ed" do
+      subject { Composer.new(t1, t2) }
+
+      it "returns the Point that results from passing its argument through all its transforms in the correct order" do
+        input = Draught::Point.new(1,2)
+        expected = Draught::Point.new(-1,-2)
+      end
+    end
+
+    context "optimisations" do
+      let(:t3) { Affine.new(Matrix[[2, 0, 0],[0, 2, 0],[0, 0, 1]]) }
+      let(:composed) { Composer.new(t1, t2) }
+      let(:input) { Draught::Point.new(1,2) }
+      let(:expected) { Draught::Point.new(-2,-4) }
+
+      it "can flatten the transforms from composed Composer objects into a single Composer array" do
+        composer = Composer.new(composed, t3)
+
+        expect(composer.flattened_transforms).to eq([t1, t2, t3])
+      end
+
+      context "coalescing transforms" do
+        let(:pt) { Proclike.new(->(p) { p }) }
+        let(:t1_t2_coalesced) { Affine.new(Matrix[[-1, 0, 0],[0, -1, 0],[0, 0, 1]]) }
+
+        context "coalescing the transforms" do
+          it "can coalesce a sequence of Affine transforms into a single transform" do
+            expect(composed.coalesced_transforms).to eq([t1_t2_coalesced])
+          end
+
+          it "copes when a non-Affine transform begins the sequence" do
+            composer = Composer.new(pt, t1, t2)
+
+            expect(composer.coalesced_transforms).to eq([pt, t1_t2_coalesced])
+          end
+
+          it "copes with an empty transforms list" do
+            composer = Composer.new()
+
+            expect(composer.coalesced_transforms).to eq([])
+          end
+
+          it "copes with a single-item transform list" do
+            composer = Composer.new(pt)
+
+            expect(composer.coalesced_transforms).to eq([pt])
+          end
+
+          it "copes when a non-Affine transform ends the sequence" do
+            composer = Composer.new(t1, t2, pt)
+
+            expect(composer.coalesced_transforms).to eq([t1_t2_coalesced, pt])
+          end
+
+          it "can coalesce either side of a non-Affine transform" do
+            composer = Composer.new(t1, t2, pt, t1, t2)
+
+            expect(composer.coalesced_transforms).to eq([t1_t2_coalesced, pt, t1_t2_coalesced])
+          end
+
+          it "copes with multiple proclike transforms" do
+            composer = Composer.new(pt, pt)
+
+            expect(composer.coalesced_transforms).to eq([pt, pt])
+          end
+        end
+
+        it "can return a coalesced version of itself" do
+          coalesced = composed.coalesce
+
+          expect(coalesced.transforms).to eq([t1_t2_coalesced])
+        end
+
+        it "offers a convenience creator that returns a coalesced composed version of its passed transforms" do
+          expect(Composer.coalesced(t1, t2)).to eq(Composer.new(t1_t2_coalesced))
+        end
+      end
+    end
+  end
+end

spec/draught/transformations/proclike_spec.rb
@@ -1,0 +1,31 @@
+require 'draught/transformations/proclike'
+require 'draught/transformations/shared_examples'
+require 'draught/point'
+
+module Draught::Transformations
+  RSpec.describe Proclike do
+    let(:array_return_transformer) { ->(p) { [2 * p.x, 2 * p.y] } }
+    let(:point_return_transformer) { ->(p) { Draught::Point.new(p.x + 10, p.y + 10) } }
+    let(:input_point) { Draught::Point.new(1,2) }
+    let(:result_point) { Draught::Point.new(11,12) }
+
+    subject { Proclike.new(point_return_transformer) }
+
+    it_should_behave_like "a well-behaved transformation class"
+
+    describe "running the transformation with #call() and passing in a Point" do
+      it "returns the result as a Point if the block returns an [x,y] tuple" do
+        expect(Proclike.new(array_return_transformer).call(input_point)).
+          to eq(Draught::Point.new(2,4))
+      end
+    end
+
+    specify "the transform is not Affine" do
+      expect(Proclike.new(array_return_transformer).affine?).to be false
+    end
+
+    specify "Proclike transforms cannot be coalesced, so raise TypeError if asked to" do
+        expect { subject.coalesce(subject) }.to raise_error(TypeError)
+    end
+  end
+end

spec/draught/transformations/shared_examples.rb
@@ -1,0 +1,28 @@
+require 'draught/transformations/composer'
+
+RSpec.shared_examples "a well-behaved transformation class" do
+  it "responds to #call(), taking a single Point argument and returning the Point result of applying the transform" do
+    expect(subject.call(input_point)).
+      to eq(result_point)
+  end
+
+  it "compares equal to a dup of itself" do
+    expect(subject.dup).to eq(subject)
+  end
+
+  context "being aware of affine-ness" do
+    it "responds to #affine?" do
+      expect(subject).to respond_to(:affine?)
+    end
+  end
+
+  context "composition" do
+    it "returns a correctly instantiated composed transform in response to #compose" do
+      expect(subject.compose(subject)).to eq(Draught::Transformations::Composer.coalesced(subject, subject))
+    end
+
+    it "returns an array consisting of itself in response to #flattened_transforms" do
+      expect(subject.flattened_transforms).to eq([subject])
+    end
+  end
+end