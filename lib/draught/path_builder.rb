fidothe/box_dividers

Add Point/Path equality and translation
Translation allows simplifying a bunch of stuff around path generation
because paths with the same origin can be connected together by
translating the second path in relation to the last point of the first.

I was actually kind of surprised by how much simpler it made the divider
generation code I was using to validate my assumptions

lib/box_dividers/path_builder.rb
@@ -1,0 +1,26 @@
+require_relative 'path'
+
+module BoxDividers
+  class PathBuilder
+    def self.build
+      builder = new
+      yield(builder)
+      builder.send(:path)
+    end
+
+    attr_reader :path
+    private :path
+
+    def initialize
+      @path = Path.new
+    end
+
+    def <<(path_or_point)
+      @path = path << path_or_point
+    end
+
+    def last
+      path.last
+    end
+  end
+end

spec/box_dividers/path_builder_spec.rb
@@ -1,0 +1,44 @@
+require 'box_dividers/point'
+require 'box_dividers/path_builder'
+
+module BoxDividers
+  RSpec.describe PathBuilder do
+    let(:p1) { Point.new(1,1) }
+    let(:p2) { Point.new(2,2) }
+
+    it "yields an object to a block and collects points appended to it into a new Path" do
+      path = PathBuilder.build { |p|
+        p << p1
+        p << p2
+      }
+
+      expect(path.points).to eq([p1, p2])
+    end
+
+    specify "the yielded value will return the last point added if asked" do
+      collector = []
+      path = PathBuilder.build { |p|
+        p << p1
+        collector << p.last
+      }
+
+      expect(collector).to eq([p1])
+    end
+
+    specify "the yielded value will correctly return the last point even when another path was appended" do
+      other_path = PathBuilder.build { |p|
+        p << p1
+        p << p2
+      }
+      collector = []
+
+      path = PathBuilder.build { |p|
+        p << p1
+        p << other_path
+        collector << p.last
+      }
+
+      expect(collector).to eq([p2])
+    end
+  end
+end

fidothe/draught

lib/draught/path_builder.rb
@@ -1,0 +1,26 @@
+require_relative 'path'
+
+module Draught
+  class PathBuilder
+    def self.build
+      builder = new
+      yield(builder)
+      builder.send(:path)
+    end
+
+    attr_reader :path
+    private :path
+
+    def initialize
+      @path = Path.new
+    end
+
+    def <<(path_or_point)
+      @path = path << path_or_point
+    end
+
+    def last
+      path.last
+    end
+  end
+end

spec/draught/path_builder_spec.rb
@@ -1,0 +1,44 @@
+require 'draught/point'
+require 'draught/path_builder'
+
+module Draught
+  RSpec.describe PathBuilder do
+    let(:p1) { Point.new(1,1) }
+    let(:p2) { Point.new(2,2) }
+
+    it "yields an object to a block and collects points appended to it into a new Path" do
+      path = PathBuilder.build { |p|
+        p << p1
+        p << p2
+      }
+
+      expect(path.points).to eq([p1, p2])
+    end
+
+    specify "the yielded value will return the last point added if asked" do
+      collector = []
+      path = PathBuilder.build { |p|
+        p << p1
+        collector << p.last
+      }
+
+      expect(collector).to eq([p1])
+    end
+
+    specify "the yielded value will correctly return the last point even when another path was appended" do
+      other_path = PathBuilder.build { |p|
+        p << p1
+        p << p2
+      }
+      collector = []
+
+      path = PathBuilder.build { |p|
+        p << p1
+        p << other_path
+        collector << p.last
+      }
+
+      expect(collector).to eq([p2])
+    end
+  end
+end