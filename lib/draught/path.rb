fidothe/box_dividers

Add immutable Path object

lib/box_dividers/path.rb
@@ -1,0 +1,25 @@
+require 'forwardable'
+
+module BoxDividers
+  class Path
+    extend Forwardable
+
+    def_delegators :@points, :empty?, :[]
+
+    def initialize(points = [])
+      @points = points.dup.freeze
+    end
+
+    def to_a
+      @points.dup
+    end
+
+    def <<(point)
+      add_points([point])
+    end
+
+    def add_points(points)
+      self.class.new(@points + points)
+    end
+  end
+end

spec/box_dividers/path_spec.rb
@@ -1,0 +1,35 @@
+require 'box_dividers/point'
+require 'box_dividers/path'
+
+module BoxDividers
+  RSpec.describe Path do
+    let(:point) { Point.new(1,1) }
+
+    it "contains no points by default" do
+      expect(subject.empty?).to be(true)
+    end
+
+    it "can be initialized with an array of Points" do
+      path = Path.new([point])
+
+      expect(path.empty?).to be(false)
+      expect(path.to_a).to eq([point])
+    end
+
+    specify "adding a point to the path returns a new path" do
+      path = subject << point
+
+      expect(path.to_a).to eq([point])
+      expect(subject).to be_empty
+    end
+
+    specify "adding several points to the path returns a new path" do
+      other_point = Point.new(2,2)
+
+      path = subject.add_points([point, other_point])
+
+      expect(path.to_a).to eq([point, other_point])
+      expect(subject).to be_empty
+    end
+  end
+end

fidothe/draught

lib/draught/path.rb
@@ -1,0 +1,25 @@
+require 'forwardable'
+
+module Draught
+  class Path
+    extend Forwardable
+
+    def_delegators :@points, :empty?, :[]
+
+    def initialize(points = [])
+      @points = points.dup.freeze
+    end
+
+    def to_a
+      @points.dup
+    end
+
+    def <<(point)
+      add_points([point])
+    end
+
+    def add_points(points)
+      self.class.new(@points + points)
+    end
+  end
+end

spec/draught/path_spec.rb
@@ -1,0 +1,35 @@
+require 'draught/point'
+require 'draught/path'
+
+module Draught
+  RSpec.describe Path do
+    let(:point) { Point.new(1,1) }
+
+    it "contains no points by default" do
+      expect(subject.empty?).to be(true)
+    end
+
+    it "can be initialized with an array of Points" do
+      path = Path.new([point])
+
+      expect(path.empty?).to be(false)
+      expect(path.to_a).to eq([point])
+    end
+
+    specify "adding a point to the path returns a new path" do
+      path = subject << point
+
+      expect(path.to_a).to eq([point])
+      expect(subject).to be_empty
+    end
+
+    specify "adding several points to the path returns a new path" do
+      other_point = Point.new(2,2)
+
+      path = subject.add_points([point, other_point])
+
+      expect(path.to_a).to eq([point, other_point])
+      expect(subject).to be_empty
+    end
+  end
+end