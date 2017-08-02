fidothe/box_dividers

Make use of Vectors for translation across the piece

lib/box_dividers/boxlike.rb
@@ -1,4 +1,5 @@
 require_relative 'point'
+require_relative 'vector'
 
 module BoxDividers
   module Boxlike
@@ -15,15 +16,15 @@ module BoxDividers
     end
 
     def lower_right
-      @lower_right ||= lower_left.translate(Point.new(width, 0))
+      @lower_right ||= lower_left.translate(BoxDividers::Vector.new(width, 0))
     end
 
     def upper_left
-      @upper_left ||= lower_left.translate(Point.new(0, height))
+      @upper_left ||= lower_left.translate(BoxDividers::Vector.new(0, height))
     end
 
     def upper_right
-      @upper_right ||= lower_left.translate(Point.new(width, height))
+      @upper_right ||= lower_left.translate(BoxDividers::Vector.new(width, height))
     end
 
     def corners
@@ -31,8 +32,8 @@ module BoxDividers
     end
 
     def move_to(point)
-      translation = lower_left.translation_to(point)
-      return self if translation == Point::ZERO
+      translation = BoxDividers::Vector.translation_between(lower_left, point)
+      return self if translation == BoxDividers::Vector::NULL
       translate(translation)
     end
 

spec/box_dividers/bounding_box_spec.rb
@@ -2,6 +2,7 @@ require 'box_dividers/boxlike_examples'
 require 'box_dividers/bounding_box'
 require 'box_dividers/transformations'
 require 'box_dividers/path'
+require 'box_dividers/vector'
 
 module BoxDividers
   RSpec.describe BoundingBox do
@@ -43,7 +44,7 @@ module BoxDividers
       it "can be translated" do
         expected = BoundingBox.new(Path.new([Point.new(0,-1), Point.new(4,3)]))
 
-        translated = subject.translate(Point.new(1,0))
+        translated = subject.translate(Vector.new(1,0))
 
         expect(translated).to eq(expected)
       end

spec/box_dividers/path_spec.rb
@@ -1,5 +1,6 @@
 require 'box_dividers/boxlike_examples'
 require 'box_dividers/point'
+require 'box_dividers/vector'
 require 'box_dividers/transformations'
 require 'box_dividers/path'
 
@@ -46,6 +47,12 @@ module BoxDividers
       path = first_path << other_path
 
       expect(path.points).to eq([point, other_point])
+    end
+
+    it "can return the first point in the path" do
+      path = Path.new([point])
+
+      expect(path.first).to be(point)
     end
 
     it "can return the last point in the path" do
@@ -107,14 +114,14 @@ module BoxDividers
     describe "translation and transformation" do
       let(:p1) { Point.new(1,1) }
       let(:p2) { Point.new(1,2) }
-      let(:p3) { Point.new(2,1) }
+      let(:vector) { Vector.new(2,1) }
 
       subject { Path.new([p1, p2]) }
 
       specify "translating a Path using a Point produces a new Path with appropriately translated Points" do
         expected = Path.new([Point.new(3,2), Point.new(3,3)])
 
-        expect(subject.translate(p3)).to eq(expected)
+        expect(subject.translate(vector)).to eq(expected)
       end
 
       specify "transforming a Path generates a new Path by applying the transformation to every Point in the Path" do

spec/box_dividers/point_spec.rb
@@ -1,4 +1,5 @@
 require 'box_dividers/point'
+require 'box_dividers/vector'
 
 module BoxDividers
   RSpec.describe Point do
@@ -38,18 +39,18 @@ module BoxDividers
 
     describe "manipulations in space" do
       specify "a Point can be translated using a second Point to produce a new Point" do
-        translation = Point.new(1,2)
+        translation = Vector.new(1,2)
 
         expect(subject.translate(translation)).to eq(Point.new(2,4))
       end
 
       specify "a Point can report the translation needed to relocate itself to a second Point" do
-        expect(subject.translation_to(Point.new(0,0))).to eq(Point.new(-1, -2))
+        expect(subject.translation_to(Point.new(0,0))).to eq(Vector.new(-1, -2))
       end
 
       specify "a Point can be transformed by a lambda-like object which takes the point and returns a new one" do
         transformer = ->(point) {
-          point.translate(Point.new(1,1))
+          point.translate(Vector.new(1,1))
         }
 
         expect(subject.transform(transformer)).to eq(Point.new(2,3))

spec/box_dividers/sheet_spec.rb
@@ -2,6 +2,7 @@ require 'box_dividers/sheet'
 require 'box_dividers/boxlike_examples'
 require 'box_dividers/spec_box'
 require 'box_dividers/point'
+require 'box_dividers/vector'
 
 module BoxDividers
   RSpec.describe Sheet do
@@ -16,9 +17,12 @@ module BoxDividers
     end
 
     context "translation" do
+      let(:point) { Point.new(100,100) }
+      let(:translation) { Vector.translation_between(Point::ZERO, point) }
+
       it "correctly translates its boxes" do
-        translated = subject.translate(Point.new(100,100))
-        expect(translated.containers).to eq([box.translate(Point.new(100,100))])
+        translated = subject.translate(translation)
+        expect(translated.containers).to eq([box.translate(translation)])
       end
     end
   end

fidothe/draught

lib/draught/boxlike.rb
@@ -1,4 +1,5 @@
 require_relative 'point'
+require_relative 'vector'
 
 module Draught
   module Boxlike
@@ -15,15 +16,15 @@ module Draught
     end
 
     def lower_right
-      @lower_right ||= lower_left.translate(Point.new(width, 0))
+      @lower_right ||= lower_left.translate(Draught::Vector.new(width, 0))
     end
 
     def upper_left
-      @upper_left ||= lower_left.translate(Point.new(0, height))
+      @upper_left ||= lower_left.translate(Draught::Vector.new(0, height))
     end
 
     def upper_right
-      @upper_right ||= lower_left.translate(Point.new(width, height))
+      @upper_right ||= lower_left.translate(Draught::Vector.new(width, height))
     end
 
     def corners
@@ -31,8 +32,8 @@ module Draught
     end
 
     def move_to(point)
-      translation = lower_left.translation_to(point)
-      return self if translation == Point::ZERO
+      translation = Draught::Vector.translation_between(lower_left, point)
+      return self if translation == Draught::Vector::NULL
       translate(translation)
     end
 

spec/draught/bounding_box_spec.rb
@@ -2,6 +2,7 @@ require 'draught/boxlike_examples'
 require 'draught/bounding_box'
 require 'draught/transformations'
 require 'draught/path'
+require 'draught/vector'
 
 module Draught
   RSpec.describe BoundingBox do
@@ -43,7 +44,7 @@ module Draught
       it "can be translated" do
         expected = BoundingBox.new(Path.new([Point.new(0,-1), Point.new(4,3)]))
 
-        translated = subject.translate(Point.new(1,0))
+        translated = subject.translate(Vector.new(1,0))
 
         expect(translated).to eq(expected)
       end

spec/draught/path_spec.rb
@@ -1,5 +1,6 @@
 require 'draught/boxlike_examples'
 require 'draught/point'
+require 'draught/vector'
 require 'draught/transformations'
 require 'draught/path'
 
@@ -46,6 +47,12 @@ module Draught
       path = first_path << other_path
 
       expect(path.points).to eq([point, other_point])
+    end
+
+    it "can return the first point in the path" do
+      path = Path.new([point])
+
+      expect(path.first).to be(point)
     end
 
     it "can return the last point in the path" do
@@ -107,14 +114,14 @@ module Draught
     describe "translation and transformation" do
       let(:p1) { Point.new(1,1) }
       let(:p2) { Point.new(1,2) }
-      let(:p3) { Point.new(2,1) }
+      let(:vector) { Vector.new(2,1) }
 
       subject { Path.new([p1, p2]) }
 
       specify "translating a Path using a Point produces a new Path with appropriately translated Points" do
         expected = Path.new([Point.new(3,2), Point.new(3,3)])
 
-        expect(subject.translate(p3)).to eq(expected)
+        expect(subject.translate(vector)).to eq(expected)
       end
 
       specify "transforming a Path generates a new Path by applying the transformation to every Point in the Path" do

spec/draught/point_spec.rb
@@ -1,4 +1,5 @@
 require 'draught/point'
+require 'draught/vector'
 
 module Draught
   RSpec.describe Point do
@@ -38,18 +39,18 @@ module Draught
 
     describe "manipulations in space" do
       specify "a Point can be translated using a second Point to produce a new Point" do
-        translation = Point.new(1,2)
+        translation = Vector.new(1,2)
 
         expect(subject.translate(translation)).to eq(Point.new(2,4))
       end
 
       specify "a Point can report the translation needed to relocate itself to a second Point" do
-        expect(subject.translation_to(Point.new(0,0))).to eq(Point.new(-1, -2))
+        expect(subject.translation_to(Point.new(0,0))).to eq(Vector.new(-1, -2))
       end
 
       specify "a Point can be transformed by a lambda-like object which takes the point and returns a new one" do
         transformer = ->(point) {
-          point.translate(Point.new(1,1))
+          point.translate(Vector.new(1,1))
         }
 
         expect(subject.transform(transformer)).to eq(Point.new(2,3))

spec/draught/sheet_spec.rb
@@ -2,6 +2,7 @@ require 'draught/sheet'
 require 'draught/boxlike_examples'
 require 'draught/spec_box'
 require 'draught/point'
+require 'draught/vector'
 
 module Draught
   RSpec.describe Sheet do
@@ -16,9 +17,12 @@ module Draught
     end
 
     context "translation" do
+      let(:point) { Point.new(100,100) }
+      let(:translation) { Vector.translation_between(Point::ZERO, point) }
+
       it "correctly translates its boxes" do
-        translated = subject.translate(Point.new(100,100))
-        expect(translated.containers).to eq([box.translate(Point.new(100,100))])
+        translated = subject.translate(translation)
+        expect(translated.containers).to eq([box.translate(translation)])
       end
     end
   end