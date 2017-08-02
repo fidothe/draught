fidothe/box_dividers

Make use of the new transformations code

spec/box_dividers/bounding_box_spec.rb
@@ -1,5 +1,6 @@
 require 'box_dividers/boxlike_examples'
 require 'box_dividers/bounding_box'
+require 'box_dividers/transformations'
 require 'box_dividers/path'
 
 module BoxDividers
@@ -21,7 +22,7 @@ module BoxDividers
       end
 
       it "does not compare equal if the other box has the same paths in a different order" do
-        reversed_path = Path.new(input_path.points.reverse)
+       reversed_path = Path.new(input_path.points.reverse)
         reversed = BoundingBox.new(reversed_path)
 
         expect(reversed).to_not eq(subject)
@@ -48,7 +49,9 @@ module BoxDividers
       end
 
       it "can be transformed" do
-        transformation = Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        transformation = BoxDividers::Transformations::Affine.new(
+          Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        )
         expected = BoundingBox.new(Path.new([Point.new(-2,-2), Point.new(6,6)]))
 
         transformed = subject.transform(transformation)

spec/box_dividers/boxlike_examples.rb
@@ -1,4 +1,5 @@
 require 'box_dividers/point'
+require 'box_dividers/transformations/affine'
 
 RSpec.shared_examples "a basic rectangular box-like thing" do
   context "the box's dimensions" do
@@ -58,7 +59,9 @@ RSpec.shared_examples "a basic rectangular box-like thing" do
     end
 
     context "transformation" do
-      let(:transformer) { Matrix[[2,0,0],[0,2,0],[0,0,1]] }
+      let(:transformer) {
+        BoxDividers::Transformations::Affine.new(Matrix[[2,0,0],[0,2,0],[0,0,1]])
+      }
       let(:transformed) { subject.transform(transformer) }
 
       specify "moves the origin of the box correctly" do

spec/box_dividers/container_spec.rb
@@ -2,6 +2,7 @@ require 'box_dividers/container'
 require 'box_dividers/boxlike_examples'
 require 'box_dividers/spec_box'
 require 'box_dividers/point'
+require 'box_dividers/transformations'
 
 module BoxDividers
   RSpec.describe Container do
@@ -16,7 +17,10 @@ module BoxDividers
 
     context "min_gap and transformation" do
       specify "we assume transformations are simply uniform and the min_gap gets scaled as if it were an x co-ord" do
-        transformed = subject.transform(Matrix[[2,0,0],[0,2,0],[0,0,1]])
+        transformation = BoxDividers::Transformations::Affine.new(
+          Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        )
+        transformed = subject.transform(transformation)
         expect(transformed.min_gap).to eq(100)
       end
     end

spec/box_dividers/path_spec.rb
@@ -1,5 +1,6 @@
 require 'box_dividers/boxlike_examples'
 require 'box_dividers/point'
+require 'box_dividers/transformations'
 require 'box_dividers/path'
 
 module BoxDividers
@@ -117,7 +118,9 @@ module BoxDividers
       end
 
       specify "transforming a Path generates a new Path by applying the transformation to every Point in the Path" do
-        transformation = Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        transformation = BoxDividers::Transformations::Affine.new(
+          Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        )
         expected = Path.new([Point.new(2,2), Point.new(2,4)])
 
         expect(subject.transform(transformation)).to eq(expected)

fidothe/draught

spec/draught/bounding_box_spec.rb
@@ -1,5 +1,6 @@
 require 'draught/boxlike_examples'
 require 'draught/bounding_box'
+require 'draught/transformations'
 require 'draught/path'
 
 module Draught
@@ -21,7 +22,7 @@ module Draught
       end
 
       it "does not compare equal if the other box has the same paths in a different order" do
-        reversed_path = Path.new(input_path.points.reverse)
+       reversed_path = Path.new(input_path.points.reverse)
         reversed = BoundingBox.new(reversed_path)
 
         expect(reversed).to_not eq(subject)
@@ -48,7 +49,9 @@ module Draught
       end
 
       it "can be transformed" do
-        transformation = Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        transformation = Draught::Transformations::Affine.new(
+          Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        )
         expected = BoundingBox.new(Path.new([Point.new(-2,-2), Point.new(6,6)]))
 
         transformed = subject.transform(transformation)

spec/draught/boxlike_examples.rb
@@ -1,4 +1,5 @@
 require 'draught/point'
+require 'draught/transformations/affine'
 
 RSpec.shared_examples "a basic rectangular box-like thing" do
   context "the box's dimensions" do
@@ -58,7 +59,9 @@ RSpec.shared_examples "a basic rectangular box-like thing" do
     end
 
     context "transformation" do
-      let(:transformer) { Matrix[[2,0,0],[0,2,0],[0,0,1]] }
+      let(:transformer) {
+        Draught::Transformations::Affine.new(Matrix[[2,0,0],[0,2,0],[0,0,1]])
+      }
       let(:transformed) { subject.transform(transformer) }
 
       specify "moves the origin of the box correctly" do

spec/draught/container_spec.rb
@@ -2,6 +2,7 @@ require 'draught/container'
 require 'draught/boxlike_examples'
 require 'draught/spec_box'
 require 'draught/point'
+require 'draught/transformations'
 
 module Draught
   RSpec.describe Container do
@@ -16,7 +17,10 @@ module Draught
 
     context "min_gap and transformation" do
       specify "we assume transformations are simply uniform and the min_gap gets scaled as if it were an x co-ord" do
-        transformed = subject.transform(Matrix[[2,0,0],[0,2,0],[0,0,1]])
+        transformation = Draught::Transformations::Affine.new(
+          Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        )
+        transformed = subject.transform(transformation)
         expect(transformed.min_gap).to eq(100)
       end
     end

spec/draught/path_spec.rb
@@ -1,5 +1,6 @@
 require 'draught/boxlike_examples'
 require 'draught/point'
+require 'draught/transformations'
 require 'draught/path'
 
 module Draught
@@ -117,7 +118,9 @@ module Draught
       end
 
       specify "transforming a Path generates a new Path by applying the transformation to every Point in the Path" do
-        transformation = Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        transformation = Draught::Transformations::Affine.new(
+          Matrix[[2,0,0],[0,2,0],[0,0,1]]
+        )
         expected = Path.new([Point.new(2,2), Point.new(2,4)])
 
         expect(subject.transform(transformation)).to eq(expected)