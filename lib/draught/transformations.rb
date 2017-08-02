fidothe/box_dividers

Add BoundingBox to group Paths and a block transform
The block transform allows a block to be applied to every point of a
path, every path of a bounding box, etc. Add sample transforms, in this
case a very simple mm-to-pt transform for making PDFs the right size

lib/box_dividers/transformations.rb
@@ -1,0 +1,13 @@
+module BoxDividers
+  module Transformations
+    extend self
+
+    MM_TO_PT = 2.8346456692913
+
+    def mm_to_pt
+      ->(x, y) {
+        [x * MM_TO_PT, y * MM_TO_PT]
+      }
+    end
+  end
+end

spec/box_dividers/transformations_spec.rb
@@ -1,0 +1,14 @@
+require 'box_dividers/transformations'
+
+module BoxDividers
+  RSpec.describe Transformations do
+    describe "units" do
+      it "can convert mm to Postscript pts" do
+        expect(Transformations.mm_to_pt.call(1, 1)).to eq([
+          2.8346456692913,
+          2.8346456692913
+        ])
+      end
+    end
+  end
+end

fidothe/draught

lib/draught/transformations.rb
@@ -1,0 +1,13 @@
+module Draught
+  module Transformations
+    extend self
+
+    MM_TO_PT = 2.8346456692913
+
+    def mm_to_pt
+      ->(x, y) {
+        [x * MM_TO_PT, y * MM_TO_PT]
+      }
+    end
+  end
+end

spec/draught/transformations_spec.rb
@@ -1,0 +1,14 @@
+require 'draught/transformations'
+
+module Draught
+  RSpec.describe Transformations do
+    describe "units" do
+      it "can convert mm to Postscript pts" do
+        expect(Transformations.mm_to_pt.call(1, 1)).to eq([
+          2.8346456692913,
+          2.8346456692913
+        ])
+      end
+    end
+  end
+end