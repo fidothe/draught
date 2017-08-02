fidothe/box_dividers

Add Vector class to represent translation vectors
A Vector Is Not A Point...

Vector class has x, y attributes, but these represent direction and
magnitude decomposed into cartesian coordinate components. (A Vector's
direction and magnitude being with respect to a cartesian 0,0 origin.)

Vectors can produce an affine transform that can be used to translate a
Point.

To accomodate this widening of the space for
things-that-can-be-used-to-transform, make some changes to the shared
examples for transformations that allow the interface tests to be
properly shared between objects.

lib/box_dividers/vector.rb
@@ -1,0 +1,37 @@
+require 'matrix'
+require_relative './transformations/affine'
+
+module BoxDividers
+  class Vector
+    def self.from_xy(x, y)
+      new(x, y)
+    end
+
+    def self.from_degrees_and_magnitude(degrees, magnitude)
+      radians = degrees * (Math::PI / 180)
+      from_radians_and_magnitude(radians, magnitude)
+    end
+
+    def self.from_radians_and_magnitude(radians, magnitude)
+      x = Math.cos(radians) * magnitude
+      y = Math.sin(radians) * magnitude
+      new(x, y)
+    end
+
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+
+    def ==(other)
+      other.respond_to?(:to_transform) && other.x == x && other.y == y
+    end
+
+    def to_transform
+      @transform ||= Transformations::Affine.new(
+        Matrix[[1, 0, x], [0, 1, y], [0, 0, 1]]
+      )
+    end
+  end
+end

spec/box_dividers/transformations/affine_spec.rb
@@ -9,11 +9,12 @@ module BoxDividers::Transformations
       Matrix[[-1, 0, 0],[0, -1, 0],[0,0,1]]
     }
     let(:input_point) { BoxDividers::Point.new(1,2) }
-    let(:result_point) { BoxDividers::Point.new(-1,-2) }
+    let(:expected_point) { BoxDividers::Point.new(-1,-2) }
 
     subject { Affine.new(transformation_matrix) }
 
-    it_should_behave_like "a well-behaved transformation class"
+    include_examples "transformation object fundamentals"
+    include_examples "producing a transform-compatible verison of itself"
 
     it "claims to be affine" do
       expect(Affine.new(transformation_matrix).affine?).to be true
@@ -42,18 +43,14 @@ module BoxDividers::Transformations
       end
     end
 
-    specify "two Affine transforms can be composed into a single one" do
-      t1 = Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]])
-      t2 = Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]])
+    context "composition" do
+      let(:other_transform) { Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]]) }
+      subject { Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]]) }
 
-      expect(t2.compose(t1).call(input_point)).to eq(BoxDividers::Point.new(-1,-2))
-    end
+      let(:input_point) { BoxDividers::Point.new(1,2) }
+      let(:expected_point) { BoxDividers::Point.new(-1,-2) }
 
-    specify "an Affine transform can be composed with a Proclike" do
-      t1 = Proclike.new(->(p) { [p.x + 2, p.y + 2] })
-      t2 = Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]])
-
-      expect(t2.compose(t1).call(input_point)).to eq(BoxDividers::Point.new(3,-4))
+      include_examples "composable with another transform"
     end
   end
 end

spec/box_dividers/transformations/composer_spec.rb
@@ -2,6 +2,7 @@ require 'box_dividers/transformations/composer'
 require 'box_dividers/transformations/affine'
 require 'box_dividers/transformations/proclike'
 require 'box_dividers/point'
+require 'box_dividers/transformations/shared_examples'
 
 module BoxDividers::Transformations
   RSpec.describe Composer do
@@ -16,13 +17,12 @@ module BoxDividers::Transformations
       expect(Composer.new(t2, t1)).to eq(Composer.new(t2, t1))
     end
 
-    context "being #call()ed" do
+    context "behaving like a transform" do
+      let(:input_point) { BoxDividers::Point.new(1,2) }
+      let(:expected_point) { BoxDividers::Point.new(-1,-2) }
       subject { Composer.new(t1, t2) }
 
-      it "returns the Point that results from passing its argument through all its transforms in the correct order" do
-        input = BoxDividers::Point.new(1,2)
-        expected = BoxDividers::Point.new(-1,-2)
-      end
+      include_examples "transformation object fundamentals"
     end
 
     context "optimisations" do

spec/box_dividers/transformations/proclike_spec.rb
@@ -7,11 +7,12 @@ module BoxDividers::Transformations
     let(:array_return_transformer) { ->(p) { [2 * p.x, 2 * p.y] } }
     let(:point_return_transformer) { ->(p) { BoxDividers::Point.new(p.x + 10, p.y + 10) } }
     let(:input_point) { BoxDividers::Point.new(1,2) }
-    let(:result_point) { BoxDividers::Point.new(11,12) }
+    let(:expected_point) { BoxDividers::Point.new(11,12) }
 
     subject { Proclike.new(point_return_transformer) }
 
-    it_should_behave_like "a well-behaved transformation class"
+    include_examples "transformation object fundamentals"
+    include_examples "producing a transform-compatible verison of itself"
 
     describe "running the transformation with #call() and passing in a Point" do
       it "returns the result as a Point if the block returns an [x,y] tuple" do
@@ -25,7 +26,14 @@ module BoxDividers::Transformations
     end
 
     specify "Proclike transforms cannot be coalesced, so raise TypeError if asked to" do
-        expect { subject.coalesce(subject) }.to raise_error(TypeError)
+      expect { subject.coalesce(subject) }.to raise_error(TypeError)
+    end
+
+    context "composition" do
+      let(:other_transform) { Proclike.new(array_return_transformer) }
+      let(:expected_point) { BoxDividers::Point.new(12,14) }
+
+      include_examples "composable with another transform"
     end
   end
 end

spec/box_dividers/vector_spec.rb
@@ -1,0 +1,73 @@
+require 'box_dividers/vector'
+require 'box_dividers/point'
+require 'box_dividers/transformations/shared_examples'
+
+module BoxDividers
+  RSpec.describe Vector do
+    let(:radians) { Math.atan(4/3.0) }
+    let(:degrees) { radians * (180.0 / Math::PI) }
+
+    it "can be created from an x,y pair" do
+      vector = Vector.from_xy(3,4)
+      expect(vector.x).to eq(3)
+      expect(vector.y).to eq(4)
+    end
+
+    it "can be created from a direction in degrees and a magnitude" do
+      vector = Vector.from_degrees_and_magnitude(degrees, 5)
+
+      expect(vector.x).to be_within(0.00001).of(3)
+      expect(vector.y).to be_within(0.00001).of(4)
+    end
+
+    it "can be created from a direction in radians and a magnitude" do
+      vector = Vector.from_radians_and_magnitude(radians, 5)
+
+      expect(vector.x).to be_within(0.00001).of(3)
+      expect(vector.y).to be_within(0.00001).of(4)
+    end
+
+    context "directions which result in negative cartesian co-ords" do
+      it "copes with reflected-x" do
+        vector = Vector.from_degrees_and_magnitude(180 - degrees, 5)
+
+        expect(vector.x).to be_within(0.00001).of(-3)
+        expect(vector.y).to be_within(0.00001).of(4)
+      end
+
+      it "copes with reflected-y" do
+        vector = Vector.from_degrees_and_magnitude(360 - degrees, 5)
+
+        expect(vector.x).to be_within(0.00001).of(3)
+        expect(vector.y).to be_within(0.00001).of(-4)
+      end
+
+      it "copes with reflected x and y" do
+        vector = Vector.from_degrees_and_magnitude(180 + degrees, 5)
+
+        expect(vector.x).to be_within(0.00001).of(-3)
+        expect(vector.y).to be_within(0.00001).of(-4)
+      end
+    end
+
+    context "comparison" do
+      subject { Vector.new(1,2) }
+
+      it "compares equal to another vector with the same x, y" do
+        expect(subject).to eq(Vector.new(1,2))
+      end
+
+      it "does not compare equal to a Point with the same x, y" do
+        expect(subject).not_to eq(Point.new(1,2))
+      end
+    end
+
+    context "Affine transformations" do
+      let(:input_point) { Point.new(3, 4) }
+      let(:expected_point) { Point.new(4, 6) }
+      subject { Vector.new(1, 2) }
+
+      include_examples "producing a transform-compatible verison of itself"
+    end
+  end
+end

fidothe/draught

lib/draught/vector.rb
@@ -1,0 +1,37 @@
+require 'matrix'
+require_relative './transformations/affine'
+
+module Draught
+  class Vector
+    def self.from_xy(x, y)
+      new(x, y)
+    end
+
+    def self.from_degrees_and_magnitude(degrees, magnitude)
+      radians = degrees * (Math::PI / 180)
+      from_radians_and_magnitude(radians, magnitude)
+    end
+
+    def self.from_radians_and_magnitude(radians, magnitude)
+      x = Math.cos(radians) * magnitude
+      y = Math.sin(radians) * magnitude
+      new(x, y)
+    end
+
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+
+    def ==(other)
+      other.respond_to?(:to_transform) && other.x == x && other.y == y
+    end
+
+    def to_transform
+      @transform ||= Transformations::Affine.new(
+        Matrix[[1, 0, x], [0, 1, y], [0, 0, 1]]
+      )
+    end
+  end
+end

spec/draught/transformations/affine_spec.rb
@@ -9,11 +9,12 @@ module Draught::Transformations
       Matrix[[-1, 0, 0],[0, -1, 0],[0,0,1]]
     }
     let(:input_point) { Draught::Point.new(1,2) }
-    let(:result_point) { Draught::Point.new(-1,-2) }
+    let(:expected_point) { Draught::Point.new(-1,-2) }
 
     subject { Affine.new(transformation_matrix) }
 
-    it_should_behave_like "a well-behaved transformation class"
+    include_examples "transformation object fundamentals"
+    include_examples "producing a transform-compatible verison of itself"
 
     it "claims to be affine" do
       expect(Affine.new(transformation_matrix).affine?).to be true
@@ -42,18 +43,14 @@ module Draught::Transformations
       end
     end
 
-    specify "two Affine transforms can be composed into a single one" do
-      t1 = Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]])
-      t2 = Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]])
+    context "composition" do
+      let(:other_transform) { Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]]) }
+      subject { Affine.new(Matrix[[-1, 0, 0],[0, 1, 0],[0, 0, 1]]) }
 
-      expect(t2.compose(t1).call(input_point)).to eq(Draught::Point.new(-1,-2))
-    end
+      let(:input_point) { Draught::Point.new(1,2) }
+      let(:expected_point) { Draught::Point.new(-1,-2) }
 
-    specify "an Affine transform can be composed with a Proclike" do
-      t1 = Proclike.new(->(p) { [p.x + 2, p.y + 2] })
-      t2 = Affine.new(Matrix[[1, 0, 0],[0, -1, 0],[0, 0, 1]])
-
-      expect(t2.compose(t1).call(input_point)).to eq(Draught::Point.new(3,-4))
+      include_examples "composable with another transform"
     end
   end
 end

spec/draught/transformations/composer_spec.rb
@@ -2,6 +2,7 @@ require 'draught/transformations/composer'
 require 'draught/transformations/affine'
 require 'draught/transformations/proclike'
 require 'draught/point'
+require 'draught/transformations/shared_examples'
 
 module Draught::Transformations
   RSpec.describe Composer do
@@ -16,13 +17,12 @@ module Draught::Transformations
       expect(Composer.new(t2, t1)).to eq(Composer.new(t2, t1))
     end
 
-    context "being #call()ed" do
+    context "behaving like a transform" do
+      let(:input_point) { Draught::Point.new(1,2) }
+      let(:expected_point) { Draught::Point.new(-1,-2) }
       subject { Composer.new(t1, t2) }
 
-      it "returns the Point that results from passing its argument through all its transforms in the correct order" do
-        input = Draught::Point.new(1,2)
-        expected = Draught::Point.new(-1,-2)
-      end
+      include_examples "transformation object fundamentals"
     end
 
     context "optimisations" do

spec/draught/transformations/proclike_spec.rb
@@ -7,11 +7,12 @@ module Draught::Transformations
     let(:array_return_transformer) { ->(p) { [2 * p.x, 2 * p.y] } }
     let(:point_return_transformer) { ->(p) { Draught::Point.new(p.x + 10, p.y + 10) } }
     let(:input_point) { Draught::Point.new(1,2) }
-    let(:result_point) { Draught::Point.new(11,12) }
+    let(:expected_point) { Draught::Point.new(11,12) }
 
     subject { Proclike.new(point_return_transformer) }
 
-    it_should_behave_like "a well-behaved transformation class"
+    include_examples "transformation object fundamentals"
+    include_examples "producing a transform-compatible verison of itself"
 
     describe "running the transformation with #call() and passing in a Point" do
       it "returns the result as a Point if the block returns an [x,y] tuple" do
@@ -25,7 +26,14 @@ module Draught::Transformations
     end
 
     specify "Proclike transforms cannot be coalesced, so raise TypeError if asked to" do
-        expect { subject.coalesce(subject) }.to raise_error(TypeError)
+      expect { subject.coalesce(subject) }.to raise_error(TypeError)
+    end
+
+    context "composition" do
+      let(:other_transform) { Proclike.new(array_return_transformer) }
+      let(:expected_point) { Draught::Point.new(12,14) }
+
+      include_examples "composable with another transform"
     end
   end
 end

spec/draught/vector_spec.rb
@@ -1,0 +1,73 @@
+require 'draught/vector'
+require 'draught/point'
+require 'draught/transformations/shared_examples'
+
+module Draught
+  RSpec.describe Vector do
+    let(:radians) { Math.atan(4/3.0) }
+    let(:degrees) { radians * (180.0 / Math::PI) }
+
+    it "can be created from an x,y pair" do
+      vector = Vector.from_xy(3,4)
+      expect(vector.x).to eq(3)
+      expect(vector.y).to eq(4)
+    end
+
+    it "can be created from a direction in degrees and a magnitude" do
+      vector = Vector.from_degrees_and_magnitude(degrees, 5)
+
+      expect(vector.x).to be_within(0.00001).of(3)
+      expect(vector.y).to be_within(0.00001).of(4)
+    end
+
+    it "can be created from a direction in radians and a magnitude" do
+      vector = Vector.from_radians_and_magnitude(radians, 5)
+
+      expect(vector.x).to be_within(0.00001).of(3)
+      expect(vector.y).to be_within(0.00001).of(4)
+    end
+
+    context "directions which result in negative cartesian co-ords" do
+      it "copes with reflected-x" do
+        vector = Vector.from_degrees_and_magnitude(180 - degrees, 5)
+
+        expect(vector.x).to be_within(0.00001).of(-3)
+        expect(vector.y).to be_within(0.00001).of(4)
+      end
+
+      it "copes with reflected-y" do
+        vector = Vector.from_degrees_and_magnitude(360 - degrees, 5)
+
+        expect(vector.x).to be_within(0.00001).of(3)
+        expect(vector.y).to be_within(0.00001).of(-4)
+      end
+
+      it "copes with reflected x and y" do
+        vector = Vector.from_degrees_and_magnitude(180 + degrees, 5)
+
+        expect(vector.x).to be_within(0.00001).of(-3)
+        expect(vector.y).to be_within(0.00001).of(-4)
+      end
+    end
+
+    context "comparison" do
+      subject { Vector.new(1,2) }
+
+      it "compares equal to another vector with the same x, y" do
+        expect(subject).to eq(Vector.new(1,2))
+      end
+
+      it "does not compare equal to a Point with the same x, y" do
+        expect(subject).not_to eq(Point.new(1,2))
+      end
+    end
+
+    context "Affine transformations" do
+      let(:input_point) { Point.new(3, 4) }
+      let(:expected_point) { Point.new(4, 6) }
+      subject { Vector.new(1, 2) }
+
+      include_examples "producing a transform-compatible verison of itself"
+    end
+  end
+end