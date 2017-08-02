fidothe/box_dividers

Points are never equal to CubicBeziers
So we need to check the type of point we're comparing during equality
checks. Introduce the #point_type method so we don't have to use
explicit class checks

spec/box_dividers/point_spec.rb
@@ -1,4 +1,5 @@
 require 'box_dividers/point'
+require 'box_dividers/cubic_bezier'
 require 'box_dividers/vector'
 
 module BoxDividers
@@ -33,6 +34,11 @@ module BoxDividers
 
         specify "a Point is not equal to another point if their co-ordinates differ" do
           expect(p1 == p3).to be(false)
+        end
+
+        specify "a Point is not equal to a CubicBezier, even if they share the same x,y" do
+          cb = CubicBezier.new(end_point: p1, control_point_1: p3, control_point_2: p1)
+          expect(p1 == cb).to be(false)
         end
 
         specify "a Point approximates another if their co-ordinates are within the specified delta" do

fidothe/draught

spec/draught/point_spec.rb
@@ -1,4 +1,5 @@
 require 'draught/point'
+require 'draught/cubic_bezier'
 require 'draught/vector'
 
 module Draught
@@ -33,6 +34,11 @@ module Draught
 
         specify "a Point is not equal to another point if their co-ordinates differ" do
           expect(p1 == p3).to be(false)
+        end
+
+        specify "a Point is not equal to a CubicBezier, even if they share the same x,y" do
+          cb = CubicBezier.new(end_point: p1, control_point_1: p3, control_point_2: p1)
+          expect(p1 == cb).to be(false)
         end
 
         specify "a Point approximates another if their co-ordinates are within the specified delta" do