fidothe/box_dividers

Add Line convenience factory
It makes two-point paths either vertically or horizontally

lib/box_dividers/line.rb
@@ -1,0 +1,16 @@
+require_relative './path'
+require_relative './point'
+
+module BoxDividers
+  class Line
+    class << self
+      def horizontal(width)
+        Path.new([Point::ZERO, Point.new(width, 0)])
+      end
+
+      def vertical(height)
+        Path.new([Point::ZERO, Point.new(0, height)])
+      end
+    end
+  end
+end

spec/box_dividers/line_spec.rb
@@ -1,0 +1,22 @@
+require 'box_dividers/path_builder'
+require 'box_dividers/line'
+
+module BoxDividers
+  RSpec.describe Line do
+    describe "generating horizontal lines" do
+      specify "a line of width N is a Path with points at (0,0) and (N,0)" do
+        expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(10, 0) }
+
+        expect(Line.horizontal(10)).to eq(expected)
+      end
+    end
+
+    describe "generating vertical lines" do
+      specify "a line of height N is a Path with points at (0,0) and (0,N)" do
+        expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(0, 10) }
+
+        expect(Line.vertical(10)).to eq(expected)
+      end
+    end
+  end
+end

fidothe/draught

lib/draught/line.rb
@@ -1,0 +1,16 @@
+require_relative './path'
+require_relative './point'
+
+module Draught
+  class Line
+    class << self
+      def horizontal(width)
+        Path.new([Point::ZERO, Point.new(width, 0)])
+      end
+
+      def vertical(height)
+        Path.new([Point::ZERO, Point.new(0, height)])
+      end
+    end
+  end
+end

spec/draught/line_spec.rb
@@ -1,0 +1,22 @@
+require 'draught/path_builder'
+require 'draught/line'
+
+module Draught
+  RSpec.describe Line do
+    describe "generating horizontal lines" do
+      specify "a line of width N is a Path with points at (0,0) and (N,0)" do
+        expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(10, 0) }
+
+        expect(Line.horizontal(10)).to eq(expected)
+      end
+    end
+
+    describe "generating vertical lines" do
+      specify "a line of height N is a Path with points at (0,0) and (0,N)" do
+        expected = PathBuilder.build { |p| p << Point::ZERO << Point.new(0, 10) }
+
+        expect(Line.vertical(10)).to eq(expected)
+      end
+    end
+  end
+end