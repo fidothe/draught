fidothe/box_dividers

join-these-lines-with-a-rounded-corner attempt 1
This is proper fake it territory. This will join two or more lines
together with a rounded corner with the following assumptions:

1. every join is a 90º corner where the incoming path runs left-to-right
and the outgoing path runs bottom-to-top.

Anything else will produce nonsensical paths

lib/box_dividers/corner.rb
@@ -1,0 +1,39 @@
+require_relative './path_builder'
+require_relative './vector'
+require_relative './arc_builder'
+
+module BoxDividers
+  module Corner
+    class Rounded
+      def self.join(args)
+        new(args).join
+      end
+
+      attr_reader :radius, :paths
+
+      def initialize(args)
+        @radius = args.fetch(:radius)
+        @paths = args.fetch(:paths)
+      end
+
+      def join
+        new_path = []
+
+        paths.inject do |incoming, outgoing|
+          incoming_pre_join_path = incoming[0..-2]
+          incoming_join_point = incoming.last.translate(Vector.new(-radius,0))
+          outgoing_post_join_path = outgoing[1..-1]
+          outgoing_join_point = outgoing.first.translate(Vector.new(0, radius))
+          corner_path = ArcBuilder.degrees(angle: 90, radius: radius, starting_angle: 270).path
+          incoming_translation = Vector.translation_between(corner_path.first, incoming_join_point)
+          intermediate_path = PathBuilder.build { |p|
+            p << incoming_pre_join_path
+            p << corner_path.translate(incoming_translation)
+          }
+          outgoing_translation = Vector.translation_between(outgoing_join_point, intermediate_path.last)
+          intermediate_path << outgoing_post_join_path.translate(outgoing_translation)
+        end
+      end
+    end
+  end
+end

spec/box_dividers/corner_spec.rb
@@ -1,0 +1,28 @@
+require 'box_dividers/corner'
+require 'box_dividers/arc_builder'
+require 'box_dividers/line'
+require 'box_dividers/path_builder'
+
+module BoxDividers
+  RSpec.describe Corner do
+    describe "rounded corners" do
+      context "joining two paths at right-angles" do
+        let(:horizontal) { Line.horizontal(10) }
+        let(:up) { Line.vertical(10) }
+        let(:down) { Line.vertical(-10) }
+
+        it "returns a new path containing the incoming path up to the point the arc starts, the arc, and the rest of the outgoing path" do
+          expected = PathBuilder.build { |p|
+            p << Point::ZERO << Point.new(9,0)
+            p << ArcBuilder.degrees(angle: 90, radius: 1, starting_angle: 270).curve.translate(Vector.new(9,0))
+            p << Point.new(10,10)
+          }
+
+          joined = Corner::Rounded.join(radius: 1, paths: [horizontal, up])
+
+          expect(joined).to approximate(expected).within(0.00001)
+        end
+      end
+    end
+  end
+end

fidothe/draught

lib/draught/corner.rb
@@ -1,0 +1,39 @@
+require_relative './path_builder'
+require_relative './vector'
+require_relative './arc_builder'
+
+module Draught
+  module Corner
+    class Rounded
+      def self.join(args)
+        new(args).join
+      end
+
+      attr_reader :radius, :paths
+
+      def initialize(args)
+        @radius = args.fetch(:radius)
+        @paths = args.fetch(:paths)
+      end
+
+      def join
+        new_path = []
+
+        paths.inject do |incoming, outgoing|
+          incoming_pre_join_path = incoming[0..-2]
+          incoming_join_point = incoming.last.translate(Vector.new(-radius,0))
+          outgoing_post_join_path = outgoing[1..-1]
+          outgoing_join_point = outgoing.first.translate(Vector.new(0, radius))
+          corner_path = ArcBuilder.degrees(angle: 90, radius: radius, starting_angle: 270).path
+          incoming_translation = Vector.translation_between(corner_path.first, incoming_join_point)
+          intermediate_path = PathBuilder.build { |p|
+            p << incoming_pre_join_path
+            p << corner_path.translate(incoming_translation)
+          }
+          outgoing_translation = Vector.translation_between(outgoing_join_point, intermediate_path.last)
+          intermediate_path << outgoing_post_join_path.translate(outgoing_translation)
+        end
+      end
+    end
+  end
+end

spec/draught/corner_spec.rb
@@ -1,0 +1,28 @@
+require 'draught/corner'
+require 'draught/arc_builder'
+require 'draught/line'
+require 'draught/path_builder'
+
+module Draught
+  RSpec.describe Corner do
+    describe "rounded corners" do
+      context "joining two paths at right-angles" do
+        let(:horizontal) { Line.horizontal(10) }
+        let(:up) { Line.vertical(10) }
+        let(:down) { Line.vertical(-10) }
+
+        it "returns a new path containing the incoming path up to the point the arc starts, the arc, and the rest of the outgoing path" do
+          expected = PathBuilder.build { |p|
+            p << Point::ZERO << Point.new(9,0)
+            p << ArcBuilder.degrees(angle: 90, radius: 1, starting_angle: 270).curve.translate(Vector.new(9,0))
+            p << Point.new(10,10)
+          }
+
+          joined = Corner::Rounded.join(radius: 1, paths: [horizontal, up])
+
+          expect(joined).to approximate(expected).within(0.00001)
+        end
+      end
+    end
+  end
+end