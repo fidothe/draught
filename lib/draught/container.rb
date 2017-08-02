fidothe/box_dividers

Add grouping objects Container and Sheet
Containers hold one BoundingBox but can have a minimum nesting gap
specified. Sheets hold mulitple Boxes and have a width/height that is
independent of their contents

lib/box_dividers/container.rb
@@ -1,0 +1,31 @@
+require_relative 'boxlike'
+require 'forwardable'
+
+module BoxDividers
+  class Container
+    extend Forwardable
+    include Boxlike
+
+    attr_reader :box, :min_gap
+
+    def_delegators :box, :lower_left, :width, :height
+
+    def initialize(box, opts = {})
+      @box = box
+      @min_gap = opts.fetch(:min_gap, 0)
+    end
+
+    def translate(point)
+      self.class.new(box.translate(point), {min_gap: min_gap})
+    end
+
+    def transform(transformer)
+      transformed_min_gap, _ = transformer.call(min_gap,0)
+      self.class.new(box.transform(transformer), {min_gap: transformed_min_gap})
+    end
+
+    def ==(other)
+      min_gap == other.min_gap && box == other.box
+    end
+  end
+end

lib/box_dividers/sheet.rb
@@ -1,0 +1,36 @@
+require_relative 'boxlike'
+require_relative 'point'
+
+module BoxDividers
+  class Sheet
+    include Boxlike
+
+    attr_reader :boxes, :lower_left, :width, :height
+
+    def initialize(opts = {})
+      @boxes = opts.fetch(:boxes)
+      @lower_left = opts.fetch(:lower_left, Point::ZERO)
+      @width = opts.fetch(:width)
+      @height = opts.fetch(:height)
+    end
+
+    def translate(point)
+      tr_lower_left = lower_left.translate(point)
+      tr_boxes = boxes.map { |box| box.translate(point) }
+      self.class.new(boxes: tr_boxes, lower_left: tr_lower_left, width: width, height: height)
+    end
+
+    def transform(transformer)
+      tr_lower_left = lower_left.transform(transformer)
+      tr_boxes = boxes.map { |box| box.transform(transformer) }
+      tr_width, tr_height = transformer.call(width, height)
+      self.class.new({
+        boxes: tr_boxes, lower_left: tr_lower_left, width: tr_width, height: tr_height
+      })
+    end
+
+    def ==(other)
+      lower_left == other.lower_left && width == other.width && height == other.height && boxes == other.boxes
+    end
+  end
+end

spec/box_dividers/container_spec.rb
@@ -1,0 +1,24 @@
+require 'box_dividers/container'
+require 'box_dividers/boxlike_examples'
+require 'box_dividers/spec_box'
+require 'box_dividers/point'
+
+module BoxDividers
+  RSpec.describe Container do
+    let(:box) { SpecBox.new(lower_left: Point::ZERO, width: 200, height: 100) }
+    subject { Container.new(box, min_gap: 50) }
+
+    it_should_behave_like "a basic rectangular box-like thing"
+
+    it "reports the minimum gap it should have between it and any other Container" do
+      expect(subject.min_gap).to eq(50)
+    end
+
+    context "min_gap and transformation" do
+      specify "we assume transformations are simply uniform and the min_gap gets scaled as if it were an x co-ord" do
+        transformed = subject.transform(->(x,y) { [2 * x, 2 * y] })
+        expect(transformed.min_gap).to eq(100)
+      end
+    end
+  end
+end

spec/box_dividers/sheet_spec.rb
@@ -1,0 +1,25 @@
+require 'box_dividers/sheet'
+require 'box_dividers/boxlike_examples'
+require 'box_dividers/spec_box'
+require 'box_dividers/point'
+
+module BoxDividers
+  RSpec.describe Sheet do
+    let(:box) { SpecBox.new(lower_left: Point.new(50,50), width: 200, height: 100) }
+    let(:boxes) { [box] }
+    subject { Sheet.new(boxes: boxes, width: 1000, height: 600) }
+
+    it_should_behave_like "a basic rectangular box-like thing"
+
+    it "has its origin at 0,0 by default" do
+      expect(subject.lower_left).to eq(Point::ZERO)
+    end
+
+    context "translation" do
+      it "correctly translates its boxes" do
+        translated = subject.translate(Point.new(100,100))
+        expect(translated.boxes).to eq([box.translate(Point.new(100,100))])
+      end
+    end
+  end
+end

fidothe/draught

lib/draught/container.rb
@@ -1,0 +1,31 @@
+require_relative 'boxlike'
+require 'forwardable'
+
+module Draught
+  class Container
+    extend Forwardable
+    include Boxlike
+
+    attr_reader :box, :min_gap
+
+    def_delegators :box, :lower_left, :width, :height
+
+    def initialize(box, opts = {})
+      @box = box
+      @min_gap = opts.fetch(:min_gap, 0)
+    end
+
+    def translate(point)
+      self.class.new(box.translate(point), {min_gap: min_gap})
+    end
+
+    def transform(transformer)
+      transformed_min_gap, _ = transformer.call(min_gap,0)
+      self.class.new(box.transform(transformer), {min_gap: transformed_min_gap})
+    end
+
+    def ==(other)
+      min_gap == other.min_gap && box == other.box
+    end
+  end
+end

lib/draught/sheet.rb
@@ -1,0 +1,36 @@
+require_relative 'boxlike'
+require_relative 'point'
+
+module Draught
+  class Sheet
+    include Boxlike
+
+    attr_reader :boxes, :lower_left, :width, :height
+
+    def initialize(opts = {})
+      @boxes = opts.fetch(:boxes)
+      @lower_left = opts.fetch(:lower_left, Point::ZERO)
+      @width = opts.fetch(:width)
+      @height = opts.fetch(:height)
+    end
+
+    def translate(point)
+      tr_lower_left = lower_left.translate(point)
+      tr_boxes = boxes.map { |box| box.translate(point) }
+      self.class.new(boxes: tr_boxes, lower_left: tr_lower_left, width: width, height: height)
+    end
+
+    def transform(transformer)
+      tr_lower_left = lower_left.transform(transformer)
+      tr_boxes = boxes.map { |box| box.transform(transformer) }
+      tr_width, tr_height = transformer.call(width, height)
+      self.class.new({
+        boxes: tr_boxes, lower_left: tr_lower_left, width: tr_width, height: tr_height
+      })
+    end
+
+    def ==(other)
+      lower_left == other.lower_left && width == other.width && height == other.height && boxes == other.boxes
+    end
+  end
+end

spec/draught/container_spec.rb
@@ -1,0 +1,24 @@
+require 'draught/container'
+require 'draught/boxlike_examples'
+require 'draught/spec_box'
+require 'draught/point'
+
+module Draught
+  RSpec.describe Container do
+    let(:box) { SpecBox.new(lower_left: Point::ZERO, width: 200, height: 100) }
+    subject { Container.new(box, min_gap: 50) }
+
+    it_should_behave_like "a basic rectangular box-like thing"
+
+    it "reports the minimum gap it should have between it and any other Container" do
+      expect(subject.min_gap).to eq(50)
+    end
+
+    context "min_gap and transformation" do
+      specify "we assume transformations are simply uniform and the min_gap gets scaled as if it were an x co-ord" do
+        transformed = subject.transform(->(x,y) { [2 * x, 2 * y] })
+        expect(transformed.min_gap).to eq(100)
+      end
+    end
+  end
+end

spec/draught/sheet_spec.rb
@@ -1,0 +1,25 @@
+require 'draught/sheet'
+require 'draught/boxlike_examples'
+require 'draught/spec_box'
+require 'draught/point'
+
+module Draught
+  RSpec.describe Sheet do
+    let(:box) { SpecBox.new(lower_left: Point.new(50,50), width: 200, height: 100) }
+    let(:boxes) { [box] }
+    subject { Sheet.new(boxes: boxes, width: 1000, height: 600) }
+
+    it_should_behave_like "a basic rectangular box-like thing"
+
+    it "has its origin at 0,0 by default" do
+      expect(subject.lower_left).to eq(Point::ZERO)
+    end
+
+    context "translation" do
+      it "correctly translates its boxes" do
+        translated = subject.translate(Point.new(100,100))
+        expect(translated.boxes).to eq([box.translate(Point.new(100,100))])
+      end
+    end
+  end
+end