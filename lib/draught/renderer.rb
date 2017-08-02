fidothe/box_dividers

Rework rendering to allow nested containers
This allows BoundingBoxes to include other BoundingBoxes, which turns
out to be very useful.

Effectively this unifies the interfaces so pathlike things behave enough
like boxlike things that they can be walked by the renderer without
having to think too hard...

lib/box_dividers/renderer.rb
@@ -1,4 +1,6 @@
 require 'prawn'
+require_relative './curve'
+require_relative './cubic_bezier'
 
 module BoxDividers
   class Renderer
@@ -17,25 +19,35 @@ module BoxDividers
 
       def draw_closed_path(path)
         points = path.points.dup
-        first_point = points.shift
         close_and_stroke do
           self.line_width = 0.003
-          move_to first_point.x, first_point.y
-          points.each do |point|
-            case point
-            when BoxDividers::CubicBezier
-              curve_to([point.end_point.x, point.end_point.y], {
-                bounds: [
-                  [point.control_point_1.x, point.control_point_1.y],
-                  [point.control_point_2.x, point.control_point_2.y]
-                ]
-              })
-            else
-              line_to point.x, point.y
-            end
+          move_to *point_tuple(points.shift)
+          points.each do |pointlike|
+            draw_pointlike(pointlike)
           end
-          line_to first_point.x, first_point.y
         end
+      end
+
+      def draw_pointlike(pointlike)
+        case pointlike
+        when BoxDividers::Curve
+          pointlike.as_cubic_beziers.each do |cubic_bezier|
+            draw_pointlike(cubic_bezier)
+          end
+        when BoxDividers::CubicBezier
+          curve_to(point_tuple(pointlike.end_point), {
+            bounds: [
+              point_tuple(pointlike.control_point_1),
+              point_tuple(pointlike.control_point_2)
+            ]
+          })
+        else
+          line_to *point_tuple(pointlike)
+        end
+      end
+
+      def point_tuple(point)
+        [point.x, point.y]
       end
     end
 
@@ -43,14 +55,14 @@ module BoxDividers
       new(sheet).render_to_file(path)
     end
 
-    attr_reader :sheet
+    attr_reader :root_box
 
-    def initialize(sheet)
-      @sheet = sheet
+    def initialize(root_box)
+      @root_box = root_box
     end
 
     def context
-      @context ||= PdfContext.new(sheet.width, sheet.height)
+      @context ||= PdfContext.new(root_box.width, root_box.height)
     end
 
     def render_to_file(path)
@@ -58,9 +70,7 @@ module BoxDividers
     end
 
     def render
-      sheet.containers.each do |container|
-        walk(container)
-      end
+      walk(root_box)
     end
 
     def render_container(container, context)
@@ -72,17 +82,11 @@ module BoxDividers
 
     private
 
-    def walk(container)
-      render_container(container, context)
-      render_paths(container.paths, context)
-      container.containers.each do |container|
-        walk(container)
-      end
-    end
-
-    def render_paths(paths, context)
-      paths.each do |path|
-        render_path(path, context)
+    def walk(box)
+      render_container(box, context) if box.box_type.include?(:container)
+      box.paths.each do |child|
+        render_path(child, context) if child.box_type.include?(:path)
+        walk(child)
       end
     end
   end

fidothe/draught

lib/draught/renderer.rb
@@ -1,4 +1,6 @@
 require 'prawn'
+require_relative './curve'
+require_relative './cubic_bezier'
 
 module Draught
   class Renderer
@@ -17,25 +19,35 @@ module Draught
 
       def draw_closed_path(path)
         points = path.points.dup
-        first_point = points.shift
         close_and_stroke do
           self.line_width = 0.003
-          move_to first_point.x, first_point.y
-          points.each do |point|
-            case point
-            when Draught::CubicBezier
-              curve_to([point.end_point.x, point.end_point.y], {
-                bounds: [
-                  [point.control_point_1.x, point.control_point_1.y],
-                  [point.control_point_2.x, point.control_point_2.y]
-                ]
-              })
-            else
-              line_to point.x, point.y
-            end
+          move_to *point_tuple(points.shift)
+          points.each do |pointlike|
+            draw_pointlike(pointlike)
           end
-          line_to first_point.x, first_point.y
         end
+      end
+
+      def draw_pointlike(pointlike)
+        case pointlike
+        when Draught::Curve
+          pointlike.as_cubic_beziers.each do |cubic_bezier|
+            draw_pointlike(cubic_bezier)
+          end
+        when Draught::CubicBezier
+          curve_to(point_tuple(pointlike.end_point), {
+            bounds: [
+              point_tuple(pointlike.control_point_1),
+              point_tuple(pointlike.control_point_2)
+            ]
+          })
+        else
+          line_to *point_tuple(pointlike)
+        end
+      end
+
+      def point_tuple(point)
+        [point.x, point.y]
       end
     end
 
@@ -43,14 +55,14 @@ module Draught
       new(sheet).render_to_file(path)
     end
 
-    attr_reader :sheet
+    attr_reader :root_box
 
-    def initialize(sheet)
-      @sheet = sheet
+    def initialize(root_box)
+      @root_box = root_box
     end
 
     def context
-      @context ||= PdfContext.new(sheet.width, sheet.height)
+      @context ||= PdfContext.new(root_box.width, root_box.height)
     end
 
     def render_to_file(path)
@@ -58,9 +70,7 @@ module Draught
     end
 
     def render
-      sheet.containers.each do |container|
-        walk(container)
-      end
+      walk(root_box)
     end
 
     def render_container(container, context)
@@ -72,17 +82,11 @@ module Draught
 
     private
 
-    def walk(container)
-      render_container(container, context)
-      render_paths(container.paths, context)
-      container.containers.each do |container|
-        walk(container)
-      end
-    end
-
-    def render_paths(paths, context)
-      paths.each do |path|
-        render_path(path, context)
+    def walk(box)
+      render_container(box, context) if box.box_type.include?(:container)
+      box.paths.each do |child|
+        render_path(child, context) if child.box_type.include?(:path)
+        walk(child)
       end
     end
   end