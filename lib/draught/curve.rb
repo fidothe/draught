fidothe/box_dividers

Add pointlike Curve to represent arbitrary curves
The curve portion of an Arc is conceptually one point-like thing (as
implemented in e.g. SVG), but may have to be represented as multiple
cubic Bezier curves (e.g. in PDF).

Curve is pointlike (representing the end point in the same way as a
CubicBezier), but can also return the one-or-more CubicBezier representation of
itself.

lib/box_dividers/curve.rb
@@ -1,0 +1,60 @@
+require_relative './pointlike'
+
+module BoxDividers
+  # An abstract representation of a curve in a pointlike fashion, in the way
+  # a CubicBezier is pointlike
+  class Curve
+    include Pointlike
+
+    attr_reader :point
+    protected :point
+
+    def initialize(args = {})
+      @point = args.fetch(:point)
+      @cubic_beziers = args.fetch(:cubic_beziers).dup.freeze
+    end
+
+    def x
+      @point.x
+    end
+
+    def y
+      @point.y
+    end
+
+    def point_type
+      :curve
+    end
+
+    def as_cubic_beziers
+      @cubic_beziers
+    end
+
+    def ==(other)
+      other.point_type == point_type && other.point == point &&
+        other.as_cubic_beziers == as_cubic_beziers
+    end
+
+    def approximates?(other, delta)
+      other.point_type == point_type &&
+        point.approximates?(other.point, delta) &&
+        as_cubic_beziers.zip(other.as_cubic_beziers).all? { |a, b|
+          a.approximates?(b, delta)
+        }
+    end
+
+    def translate(vector)
+      self.class.new({
+        point: @point.translate(vector),
+        cubic_beziers: @cubic_beziers.map { |c| c.translate(vector) }
+      })
+    end
+
+    def transform(transformer)
+      self.class.new({
+        point: @point.transform(transformer),
+        cubic_beziers: @cubic_beziers.map { |c| c.transform(transformer) }
+      })
+    end
+  end
+end

lib/box_dividers/pointlike.rb
@@ -1,0 +1,35 @@
+module BoxDividers
+  module Pointlike
+    def x
+      raise NotImplementedError, "including classes must return an x value"
+    end
+
+    def y
+      raise NotImplementedError, "including classes must return an y value"
+    end
+
+    def point_type
+      raise NotImplementedError, "including classes must return a Symbol with their point type"
+    end
+
+    def ==(other)
+      raise NotImplementedError, "including classes must implement equality checking. It's assumed other point_types are always unequal"
+    end
+
+    def approximates?(other, delta)
+      raise NotImplementedError, "including classes must implement approximate equality checking. It's assumed other point_types are always unequal"
+    end
+
+    def translate(vector)
+      raise NotImplementedError, "including classes must return a new instance translated by the vector arg"
+    end
+
+    def transform(transformer)
+      raise NotImplementedError, "including classes must return a new instance transformed by the Affine transform or lambda Point-based transform supplied"
+    end
+
+    def points
+      [self]
+    end
+  end
+end

spec/box_dividers/cubic_bezier_spec.rb
@@ -1,4 +1,5 @@
 require 'box_dividers/cubic_bezier'
+require 'box_dividers/pointlike_examples'
 
 module BoxDividers
   RSpec.describe CubicBezier do
@@ -10,6 +11,8 @@ module BoxDividers
     }
 
     subject { CubicBezier.new(curve_opts) }
+
+    it_behaves_like "a point-like thing"
 
     it "has an end point" do
       expect(subject.end_point).to eq(end_point)

spec/box_dividers/curve_spec.rb
@@ -1,0 +1,50 @@
+require 'box_dividers/curve'
+require 'box_dividers/point'
+require 'box_dividers/cubic_bezier'
+require 'box_dividers/pointlike_examples'
+
+module BoxDividers
+  RSpec.describe Curve do
+    let(:end_point) { Point.new(12,21) }
+    let(:cubic_bezier) {
+      CubicBezier.new({
+        end_point: end_point, control_point_1: Point.new(2,2), control_point_2: Point.new(8,8)
+      })
+    }
+    subject {
+      Curve.new(point: end_point, cubic_beziers: [cubic_bezier])
+    }
+
+    it_behaves_like "a point-like thing"
+
+    it "returns the x of its endpoint for #x" do
+      expect(subject.x).to eq(12)
+    end
+
+    it "returns the y of its endpoint for #y" do
+      expect(subject.y).to eq(21)
+    end
+
+    it "claims to a point of type curve" do
+      expect(subject.point_type).to eq(:curve)
+    end
+
+    it "returns its cubic_beziers for #as_cubic_beziers" do
+      expect(subject.as_cubic_beziers).to eq([cubic_bezier])
+    end
+
+    describe "manipulation in space" do
+      it "translates correctly" do
+        t = Vector.new(1,2)
+        expected_point = end_point.translate(t)
+        expected_cubic = cubic_bezier.translate(t)
+
+        translated = subject.translate(t)
+
+        expect(translated.x).to eq(expected_point.x)
+        expect(translated.y).to eq(expected_point.y)
+        expect(translated.as_cubic_beziers).to eq([expected_cubic])
+      end
+    end
+  end
+end

spec/box_dividers/point_spec.rb
@@ -1,10 +1,13 @@
 require 'box_dividers/point'
+require 'box_dividers/pointlike_examples'
 require 'box_dividers/cubic_bezier'
 require 'box_dividers/vector'
 
 module BoxDividers
   RSpec.describe Point do
     subject { Point.new(1, 2) }
+
+    it_behaves_like "a point-like thing"
 
     it "returns its x" do
       expect(subject.x).to eq(1)
@@ -14,38 +17,8 @@ module BoxDividers
       expect(subject.y).to eq(2)
     end
 
-    it "can return itself as an array of points" do
-      expect(subject.points).to eq([subject])
-    end
-
     specify "provides a (0,0) point via a constant" do
       expect(Point::ZERO).to eq(Point.new(0,0))
-    end
-
-    describe "comparisons" do
-      context "equality" do
-        let(:p1) { Point.new(1,1) }
-        let(:p2) { Point.new(1,1) }
-        let(:p3) { Point.new(1,2) }
-
-        specify "a Point is equal to another point if they have the same x,y co-ordinates" do
-          expect(p1 == p2).to be(true)
-        end
-
-        specify "a Point is not equal to another point if their co-ordinates differ" do
-          expect(p1 == p3).to be(false)
-        end
-
-        specify "a Point is not equal to a CubicBezier, even if they share the same x,y" do
-          cb = CubicBezier.new(end_point: p1, control_point_1: p3, control_point_2: p1)
-          expect(p1 == cb).to be(false)
-        end
-
-        specify "a Point approximates another if their co-ordinates are within the specified delta" do
-          approx_point = Point.new(1.000001, 1.000001)
-          expect(p1.approximates?(approx_point, 0.00001)).to be(true)
-        end
-      end
     end
 
     describe "manipulations in space" do

spec/box_dividers/pointlike_examples.rb
@@ -1,0 +1,83 @@
+require 'box_dividers/transformations'
+require 'box_dividers/vector'
+require 'box_dividers/point'
+
+RSpec.shared_examples "a point-like thing" do
+  it "has a numeric x value" do
+    expect(subject.x).to be_a(Numeric)
+  end
+
+  it "has a numeric y value" do
+    expect(subject.x).to be_a(Numeric)
+  end
+
+  it "has a point_type" do
+    expect(subject).to respond_to(:point_type)
+  end
+
+  it "returns a single-item array containing itself when asked for #points" do
+    expect(subject.points).to eq([subject])
+  end
+
+  describe "comparisons" do
+    context "equality" do
+      specify "one is equal to another if they have identical attributes" do
+        duplicate = subject.translate(BoxDividers::Vector::NULL)
+
+        expect(subject == duplicate).to be(true)
+      end
+
+      specify "one is not equal to another if the other has been translated even a little bit" do
+        duplicate = subject.translate(BoxDividers::Vector.new(0, 0.000001))
+
+        expect(subject == duplicate).to be(false)
+      end
+
+      specify "one is not equal to another if the other has a different point_type" do
+        ringer = subject.translate(BoxDividers::Vector::NULL)
+        orig_point_type = ringer.point_type
+        ringer_point_type = ->() { :"#{orig_point_type}#{orig_point_type}" }
+
+        ringer.define_singleton_method(:point_type, &ringer_point_type)
+
+        expect(subject == ringer).to be(false)
+      end
+
+      specify "one approximates another if their co-ordinates are within the specified delta" do
+        approximated = subject.translate(BoxDividers::Vector.new(0.000001, 0.000001))
+
+        expect(subject.approximates?(approximated, 0.00001)).to be(true)
+      end
+
+      specify "one does not approximate another if the other has a different point_type" do
+        ringer = subject.translate(BoxDividers::Vector::NULL)
+        orig_point_type = ringer.point_type
+        ringer_point_type = ->() { :"#{orig_point_type}#{orig_point_type}" }
+
+        ringer.define_singleton_method(:point_type, &ringer_point_type)
+
+        expect(subject.approximates?(ringer, 0.1)).to be(false)
+      end
+    end
+  end
+
+  describe "manipulations in space" do
+    specify "can be translated using a Vector to produce a new instance" do
+      translation = BoxDividers::Vector.new(1,2)
+
+      expect(subject.translate(translation)).to be_a(described_class)
+    end
+
+    specify "can be transformed by an Affine transform" do
+      expect(subject.transform(BoxDividers::Transformations.x_axis_reflect)).to be_a(described_class)
+    end
+
+    specify "can be transformed by a lambda operating on Points" do
+      transformer = ->(point) {
+        BoxDividers::Point.new(point.x + 1, point.y + 1)
+      }
+
+      expect(subject.transform(transformer)).to be_a(described_class)
+    end
+  end
+end

fidothe/draught

lib/draught/curve.rb
@@ -1,0 +1,60 @@
+require_relative './pointlike'
+
+module Draught
+  # An abstract representation of a curve in a pointlike fashion, in the way
+  # a CubicBezier is pointlike
+  class Curve
+    include Pointlike
+
+    attr_reader :point
+    protected :point
+
+    def initialize(args = {})
+      @point = args.fetch(:point)
+      @cubic_beziers = args.fetch(:cubic_beziers).dup.freeze
+    end
+
+    def x
+      @point.x
+    end
+
+    def y
+      @point.y
+    end
+
+    def point_type
+      :curve
+    end
+
+    def as_cubic_beziers
+      @cubic_beziers
+    end
+
+    def ==(other)
+      other.point_type == point_type && other.point == point &&
+        other.as_cubic_beziers == as_cubic_beziers
+    end
+
+    def approximates?(other, delta)
+      other.point_type == point_type &&
+        point.approximates?(other.point, delta) &&
+        as_cubic_beziers.zip(other.as_cubic_beziers).all? { |a, b|
+          a.approximates?(b, delta)
+        }
+    end
+
+    def translate(vector)
+      self.class.new({
+        point: @point.translate(vector),
+        cubic_beziers: @cubic_beziers.map { |c| c.translate(vector) }
+      })
+    end
+
+    def transform(transformer)
+      self.class.new({
+        point: @point.transform(transformer),
+        cubic_beziers: @cubic_beziers.map { |c| c.transform(transformer) }
+      })
+    end
+  end
+end

lib/draught/pointlike.rb
@@ -1,0 +1,35 @@
+module Draught
+  module Pointlike
+    def x
+      raise NotImplementedError, "including classes must return an x value"
+    end
+
+    def y
+      raise NotImplementedError, "including classes must return an y value"
+    end
+
+    def point_type
+      raise NotImplementedError, "including classes must return a Symbol with their point type"
+    end
+
+    def ==(other)
+      raise NotImplementedError, "including classes must implement equality checking. It's assumed other point_types are always unequal"
+    end
+
+    def approximates?(other, delta)
+      raise NotImplementedError, "including classes must implement approximate equality checking. It's assumed other point_types are always unequal"
+    end
+
+    def translate(vector)
+      raise NotImplementedError, "including classes must return a new instance translated by the vector arg"
+    end
+
+    def transform(transformer)
+      raise NotImplementedError, "including classes must return a new instance transformed by the Affine transform or lambda Point-based transform supplied"
+    end
+
+    def points
+      [self]
+    end
+  end
+end

spec/draught/cubic_bezier_spec.rb
@@ -1,4 +1,5 @@
 require 'draught/cubic_bezier'
+require 'draught/pointlike_examples'
 
 module Draught
   RSpec.describe CubicBezier do
@@ -10,6 +11,8 @@ module Draught
     }
 
     subject { CubicBezier.new(curve_opts) }
+
+    it_behaves_like "a point-like thing"
 
     it "has an end point" do
       expect(subject.end_point).to eq(end_point)

spec/draught/curve_spec.rb
@@ -1,0 +1,50 @@
+require 'draught/curve'
+require 'draught/point'
+require 'draught/cubic_bezier'
+require 'draught/pointlike_examples'
+
+module Draught
+  RSpec.describe Curve do
+    let(:end_point) { Point.new(12,21) }
+    let(:cubic_bezier) {
+      CubicBezier.new({
+        end_point: end_point, control_point_1: Point.new(2,2), control_point_2: Point.new(8,8)
+      })
+    }
+    subject {
+      Curve.new(point: end_point, cubic_beziers: [cubic_bezier])
+    }
+
+    it_behaves_like "a point-like thing"
+
+    it "returns the x of its endpoint for #x" do
+      expect(subject.x).to eq(12)
+    end
+
+    it "returns the y of its endpoint for #y" do
+      expect(subject.y).to eq(21)
+    end
+
+    it "claims to a point of type curve" do
+      expect(subject.point_type).to eq(:curve)
+    end
+
+    it "returns its cubic_beziers for #as_cubic_beziers" do
+      expect(subject.as_cubic_beziers).to eq([cubic_bezier])
+    end
+
+    describe "manipulation in space" do
+      it "translates correctly" do
+        t = Vector.new(1,2)
+        expected_point = end_point.translate(t)
+        expected_cubic = cubic_bezier.translate(t)
+
+        translated = subject.translate(t)
+
+        expect(translated.x).to eq(expected_point.x)
+        expect(translated.y).to eq(expected_point.y)
+        expect(translated.as_cubic_beziers).to eq([expected_cubic])
+      end
+    end
+  end
+end

spec/draught/point_spec.rb
@@ -1,10 +1,13 @@
 require 'draught/point'
+require 'draught/pointlike_examples'
 require 'draught/cubic_bezier'
 require 'draught/vector'
 
 module Draught
   RSpec.describe Point do
     subject { Point.new(1, 2) }
+
+    it_behaves_like "a point-like thing"
 
     it "returns its x" do
       expect(subject.x).to eq(1)
@@ -14,38 +17,8 @@ module Draught
       expect(subject.y).to eq(2)
     end
 
-    it "can return itself as an array of points" do
-      expect(subject.points).to eq([subject])
-    end
-
     specify "provides a (0,0) point via a constant" do
       expect(Point::ZERO).to eq(Point.new(0,0))
-    end
-
-    describe "comparisons" do
-      context "equality" do
-        let(:p1) { Point.new(1,1) }
-        let(:p2) { Point.new(1,1) }
-        let(:p3) { Point.new(1,2) }
-
-        specify "a Point is equal to another point if they have the same x,y co-ordinates" do
-          expect(p1 == p2).to be(true)
-        end
-
-        specify "a Point is not equal to another point if their co-ordinates differ" do
-          expect(p1 == p3).to be(false)
-        end
-
-        specify "a Point is not equal to a CubicBezier, even if they share the same x,y" do
-          cb = CubicBezier.new(end_point: p1, control_point_1: p3, control_point_2: p1)
-          expect(p1 == cb).to be(false)
-        end
-
-        specify "a Point approximates another if their co-ordinates are within the specified delta" do
-          approx_point = Point.new(1.000001, 1.000001)
-          expect(p1.approximates?(approx_point, 0.00001)).to be(true)
-        end
-      end
     end
 
     describe "manipulations in space" do

spec/draught/pointlike_examples.rb
@@ -1,0 +1,83 @@
+require 'draught/transformations'
+require 'draught/vector'
+require 'draught/point'
+
+RSpec.shared_examples "a point-like thing" do
+  it "has a numeric x value" do
+    expect(subject.x).to be_a(Numeric)
+  end
+
+  it "has a numeric y value" do
+    expect(subject.x).to be_a(Numeric)
+  end
+
+  it "has a point_type" do
+    expect(subject).to respond_to(:point_type)
+  end
+
+  it "returns a single-item array containing itself when asked for #points" do
+    expect(subject.points).to eq([subject])
+  end
+
+  describe "comparisons" do
+    context "equality" do
+      specify "one is equal to another if they have identical attributes" do
+        duplicate = subject.translate(Draught::Vector::NULL)
+
+        expect(subject == duplicate).to be(true)
+      end
+
+      specify "one is not equal to another if the other has been translated even a little bit" do
+        duplicate = subject.translate(Draught::Vector.new(0, 0.000001))
+
+        expect(subject == duplicate).to be(false)
+      end
+
+      specify "one is not equal to another if the other has a different point_type" do
+        ringer = subject.translate(Draught::Vector::NULL)
+        orig_point_type = ringer.point_type
+        ringer_point_type = ->() { :"#{orig_point_type}#{orig_point_type}" }
+
+        ringer.define_singleton_method(:point_type, &ringer_point_type)
+
+        expect(subject == ringer).to be(false)
+      end
+
+      specify "one approximates another if their co-ordinates are within the specified delta" do
+        approximated = subject.translate(Draught::Vector.new(0.000001, 0.000001))
+
+        expect(subject.approximates?(approximated, 0.00001)).to be(true)
+      end
+
+      specify "one does not approximate another if the other has a different point_type" do
+        ringer = subject.translate(Draught::Vector::NULL)
+        orig_point_type = ringer.point_type
+        ringer_point_type = ->() { :"#{orig_point_type}#{orig_point_type}" }
+
+        ringer.define_singleton_method(:point_type, &ringer_point_type)
+
+        expect(subject.approximates?(ringer, 0.1)).to be(false)
+      end
+    end
+  end
+
+  describe "manipulations in space" do
+    specify "can be translated using a Vector to produce a new instance" do
+      translation = Draught::Vector.new(1,2)
+
+      expect(subject.translate(translation)).to be_a(described_class)
+    end
+
+    specify "can be transformed by an Affine transform" do
+      expect(subject.transform(Draught::Transformations.x_axis_reflect)).to be_a(described_class)
+    end
+
+    specify "can be transformed by a lambda operating on Points" do
+      transformer = ->(point) {
+        Draught::Point.new(point.x + 1, point.y + 1)
+      }
+
+      expect(subject.transform(transformer)).to be_a(described_class)
+    end
+  end
+end