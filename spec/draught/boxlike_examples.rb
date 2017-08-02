fidothe/box_dividers

Add a much more useful equality test for Boxlikes
By translating the Boxlike by (0,0) we should get a new object that is
functionally identical to the first, making it a great equality test
that is properly generic without being limiting.

spec/box_dividers/boxlike_examples.rb
@@ -98,8 +98,8 @@ RSpec.shared_examples "a basic rectangular box-like thing" do
     end
 
     context "equality" do
-      it "compares equal to itself, at least" do
-        expect(subject).to eq(subject)
+      it "compares equal to a (0,0) translation of itself" do
+        expect(subject.translate(BoxDividers::Point::ZERO)).to eq(subject)
       end
     end
   end

fidothe/draught

spec/draught/boxlike_examples.rb
@@ -98,8 +98,8 @@ RSpec.shared_examples "a basic rectangular box-like thing" do
     end
 
     context "equality" do
-      it "compares equal to itself, at least" do
-        expect(subject).to eq(subject)
+      it "compares equal to a (0,0) translation of itself" do
+        expect(subject.translate(Draught::Point::ZERO)).to eq(subject)
       end
     end
   end