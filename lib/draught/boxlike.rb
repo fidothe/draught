fidothe/box_dividers

Add centre and centre-line points to boxes
which makes it easy to centre one thing relative to another using
translations

lib/box_dividers/boxlike.rb
@@ -19,12 +19,32 @@ module BoxDividers
       @lower_right ||= lower_left.translate(BoxDividers::Vector.new(width, 0))
     end
 
+    def upper_right
+      @upper_right ||= lower_left.translate(BoxDividers::Vector.new(width, height))
+    end
+
     def upper_left
       @upper_left ||= lower_left.translate(BoxDividers::Vector.new(0, height))
     end
 
-    def upper_right
-      @upper_right ||= lower_left.translate(BoxDividers::Vector.new(width, height))
+    def centre_left
+      @centre_left ||= lower_left.translate(BoxDividers::Vector.new(0, height/2.0))
+    end
+
+    def lower_centre
+      @lower_centre ||= lower_left.translate(BoxDividers::Vector.new(width/2.0, 0))
+    end
+
+    def centre_right
+      @centre_right ||= lower_right.translate(BoxDividers::Vector.new(0, height / 2.0))
+    end
+
+    def upper_centre
+      @upper_centre ||= upper_left.translate(BoxDividers::Vector.new(width/2.0, 0))
+    end
+
+    def centre
+      @centre ||= lower_left.translate(BoxDividers::Vector.new(width/2.0, height/2.0))
     end
 
     def corners

fidothe/draught

lib/draught/boxlike.rb
@@ -19,12 +19,32 @@ module Draught
       @lower_right ||= lower_left.translate(Draught::Vector.new(width, 0))
     end
 
+    def upper_right
+      @upper_right ||= lower_left.translate(Draught::Vector.new(width, height))
+    end
+
     def upper_left
       @upper_left ||= lower_left.translate(Draught::Vector.new(0, height))
     end
 
-    def upper_right
-      @upper_right ||= lower_left.translate(Draught::Vector.new(width, height))
+    def centre_left
+      @centre_left ||= lower_left.translate(Draught::Vector.new(0, height/2.0))
+    end
+
+    def lower_centre
+      @lower_centre ||= lower_left.translate(Draught::Vector.new(width/2.0, 0))
+    end
+
+    def centre_right
+      @centre_right ||= lower_right.translate(Draught::Vector.new(0, height / 2.0))
+    end
+
+    def upper_centre
+      @upper_centre ||= upper_left.translate(Draught::Vector.new(width/2.0, 0))
+    end
+
+    def centre
+      @centre ||= lower_left.translate(Draught::Vector.new(width/2.0, height/2.0))
     end
 
     def corners