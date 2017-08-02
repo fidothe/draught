fidothe/box_dividers

Add Arcs (through cubic beziers)
Add Arc class to build arcs of circles of any sweep and radius. This
uses 90º cubic bezier sections, based on reading around on the internet,
and on Prawn's internals.

Add an approximate comparison method to points and paths, mainly to make
testing easier - floating point plus several transforms means that exact
equality is not a great comparison here.

Add Scale and Rotation transforms to the transformations library, drawn
from Wikipedia's Affine Transforms page.

lib/box_dividers/arc.rb
@@ -1,0 +1,129 @@
+require_relative './point'
+require_relative './vector'
+require_relative './cubic_bezier'
+require_relative './path'
+require_relative './transformations'
+
+module BoxDividers
+  class Arc
+    LARGEST_SEGMENT_RADIANS = Math::PI / 2.0
+    CIRCLE_RADIANS = Math::PI * 2
+
+    attr_reader :radius, :starting_angle, :radians
+
+    def self.radians(degrees)
+      degrees * (Math::PI / 180)
+    end
+
+    def self.build(args = {})
+      if args.has_key?(:degrees)
+        args = args.merge({radians: radians(args[:degrees])})
+      end
+      new(args)
+    end
+
+    def initialize(args = {})
+      @radius = args.fetch(:radius)
+      @starting_angle = args.fetch(:starting_angle, 0)
+      @radians = args.fetch(:radians)
+    end
+
+    def path
+      @path ||= begin
+        remaining_angle = positive_radians
+        start = starting_angle
+        points = []
+        while remaining_angle > LARGEST_SEGMENT_RADIANS
+          remaining_angle = remaining_angle - LARGEST_SEGMENT_RADIANS
+          generate_segment(LARGEST_SEGMENT_RADIANS, start, points)
+          start = radians + starting_angle - remaining_angle
+        end
+        generate_segment(remaining_angle, start, points)
+        generate_path(points)
+      end
+    end
+
+    private
+
+    def positive_radians
+      radians.abs
+    end
+
+    def generate_segment(sweep, start, points)
+      segment = SegmentBuilder.new(sweep, start, radius)
+      points << segment.first_point if points.empty?
+      points << segment.cubic_bezier
+    end
+
+    def generate_path(points)
+      if positive?
+        Path.new(points)
+      else
+        Path.new(points).transform(Transformations.x_axis_reflect)
+      end
+    end
+
+    def positive?
+      radians >= 0
+    end
+
+    # @api: private
+    # Based on learnings from http://www.tinaja.com/glib/bezcirc2.pdf,
+    # via http://www.whizkidtech.redprince.net/bezier/circle/
+    class SegmentBuilder
+      attr_reader :sweep, :start, :radius
+
+      def initialize(sweep, start, radius)
+        @sweep, @start, @radius = sweep, start, radius
+      end
+
+      def first_point
+        Point.new(x0, -y0).transform(transform)
+      end
+
+      def cubic_bezier
+        CubicBezier.new({
+          end_point: end_point, control_point_1: control_point_1, control_point_2: control_point_2
+        }).transform(transform)
+      end
+
+      def transform
+        @transform ||= Transformations::Composer.compose(
+          Transformations.rotate(start + sweep_offset), Transformations.scale(radius)
+        )
+      end
+
+      def sweep_offset
+        @sweep_offset ||= sweep / 2.0
+      end
+
+      def end_point
+        Point.new(x0, y0)
+      end
+
+      def control_point_1
+        Point.new(x1, -y1)
+      end
+
+      def control_point_2
+        Point.new(x1, y1)
+      end
+
+      def x0
+        Math.cos(sweep_offset)
+      end
+
+      def y0
+        Math.sin(sweep_offset)
+      end
+
+      def x1
+        (4 - x0)/3.0
+      end
+
+      def y1
+        ((1 - x0) * (3 - x0)) / (3 * y0)
+      end
+    end
+  end
+end

spec/box_dividers/arc_spec.rb
@@ -1,0 +1,145 @@
+require 'box_dividers/arc'
+
+module BoxDividers
+  RSpec.describe Arc do
+    def deg_to_rad(degrees)
+      degrees * (Math::PI / 180)
+    end
+
+    let(:degrees) { 90 }
+    let(:radians) { deg_to_rad(degrees) }
+    subject { Arc.new(radius: 100, radians: radians) }
+
+    it "defaults to a starting angle of 0 radians" do
+      expect(subject.starting_angle).to eq(0)
+    end
+
+    it "reports its total angle in radians" do
+      expect(subject.radians).to eq(radians)
+    end
+
+    it "reports its radius" do
+      expect(subject.radius).to eq(100)
+    end
+
+    context "generated path" do
+      it "generates a one-segment curve for a 90º arc" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          })
+        ])
+
+        expect(subject.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a two-segment curve for a 180º arc" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-100, 0), control_point_1: Point.new(-55.22847, 100),
+            control_point_2: Point.new(-100, 55.22847)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(180))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a three-segment curve for a 270º arc" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-100, 0), control_point_1: Point.new(-55.22847, 100),
+            control_point_2: Point.new(-100, 55.22847)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(0, -100), control_point_1: Point.new(-100, -55.22847),
+            control_point_2: Point.new(-55.22847, -100)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(270))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a four-segment curve for a 360º arc" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-100, 0), control_point_1: Point.new(-55.22847, 100),
+            control_point_2: Point.new(-100, 55.22847)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(0, -100), control_point_1: Point.new(-100, -55.22847),
+            control_point_2: Point.new(-55.22847, -100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(100, 0), control_point_1: Point.new(55.22847, -100),
+            control_point_2: Point.new(100, -55.22847)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(360))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a two-segment curve for an arc between 90 and 180º" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-17.36482, 98.48078),
+            control_point_1: Point.new( -5.82146, 100),
+            control_point_2: Point.new(-11.63179, 99.49166)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(100))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a clockwise arc if a negative angle is used" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, -100), control_point_1: Point.new(100, -55.22847),
+            control_point_2: Point.new(55.22847, -100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-100, 0), control_point_1: Point.new(-55.22847, -100),
+            control_point_2: Point.new(-100, -55.22847)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(-100, 55.22847),
+            control_point_2: Point.new(-55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(100, 0), control_point_1: Point.new(55.22847, 100),
+            control_point_2: Point.new(100, 55.22847)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(-360))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+    end
+  end
+end

spec/box_dividers/path_spec.rb
@@ -49,47 +49,40 @@ module BoxDividers
       expect(path.points).to eq([point, other_point])
     end
 
-    it "can return the first point in the path" do
-      path = Path.new([point])
-
-      expect(path.first).to be(point)
-    end
-
-    it "can return the last point in the path" do
-      path = Path.new([point])
-
-      expect(path.last).to be(point)
-    end
-
-    it "can return the number of points in the path" do
-      path = Path.new([point])
-
-      expect(path.length).to eq(1)
-    end
-
-    describe "equality" do
+    describe "comparison" do
       let(:p1) { Point.new(1,1) }
       let(:p2) { Point.new(1,2) }
       let(:p3) { Point.new(2,1) }
 
       subject { Path.new([p1, p2]) }
 
-      it "compares two Paths equal if all their Points are equal" do
-        path = Path.new([p1, p2])
+      context "equality" do
+        it "compares two Paths equal if all their Points are equal" do
+          path = Path.new([p1, p2])
 
-        expect(subject == path).to be(true)
+          expect(subject == path).to be(true)
+        end
+
+        it "compares two Paths unequal if one has a different number of Points" do
+          path = subject << p3
+
+          expect(subject == path).to be(false)
+        end
+
+        it "compares two Paths unequal if one or more of their Points are not equal" do
+          path = Path.new([p1, p3])
+
+          expect(subject == path).to be(false)
+        end
+
+        it "compares equal to a (0,0) translation of itself" do
+          expect(subject.translate(BoxDividers::Vector.new(0,0))).to eq(subject)
+        end
       end
 
-      it "compares two Paths unequal if one has a different number of Points" do
-        path = subject << p3
-
-        expect(subject == path).to be(false)
-      end
-
-      it "compares two Paths unequal if one or more of their Points are not equal" do
-        path = Path.new([p1, p3])
-
-        expect(subject == path).to be(false)
+      it "compares two Paths approximately equal if all their Points are approximately equal" do
+        approx_path = subject.translate(Vector.new(0.000001, 0.000001))
+        expect(subject.approximates?(approx_path, 0.00001)).to be(true)
       end
     end
 

fidothe/draught

lib/draught/arc.rb
@@ -1,0 +1,129 @@
+require_relative './point'
+require_relative './vector'
+require_relative './cubic_bezier'
+require_relative './path'
+require_relative './transformations'
+
+module Draught
+  class Arc
+    LARGEST_SEGMENT_RADIANS = Math::PI / 2.0
+    CIRCLE_RADIANS = Math::PI * 2
+
+    attr_reader :radius, :starting_angle, :radians
+
+    def self.radians(degrees)
+      degrees * (Math::PI / 180)
+    end
+
+    def self.build(args = {})
+      if args.has_key?(:degrees)
+        args = args.merge({radians: radians(args[:degrees])})
+      end
+      new(args)
+    end
+
+    def initialize(args = {})
+      @radius = args.fetch(:radius)
+      @starting_angle = args.fetch(:starting_angle, 0)
+      @radians = args.fetch(:radians)
+    end
+
+    def path
+      @path ||= begin
+        remaining_angle = positive_radians
+        start = starting_angle
+        points = []
+        while remaining_angle > LARGEST_SEGMENT_RADIANS
+          remaining_angle = remaining_angle - LARGEST_SEGMENT_RADIANS
+          generate_segment(LARGEST_SEGMENT_RADIANS, start, points)
+          start = radians + starting_angle - remaining_angle
+        end
+        generate_segment(remaining_angle, start, points)
+        generate_path(points)
+      end
+    end
+
+    private
+
+    def positive_radians
+      radians.abs
+    end
+
+    def generate_segment(sweep, start, points)
+      segment = SegmentBuilder.new(sweep, start, radius)
+      points << segment.first_point if points.empty?
+      points << segment.cubic_bezier
+    end
+
+    def generate_path(points)
+      if positive?
+        Path.new(points)
+      else
+        Path.new(points).transform(Transformations.x_axis_reflect)
+      end
+    end
+
+    def positive?
+      radians >= 0
+    end
+
+    # @api: private
+    # Based on learnings from http://www.tinaja.com/glib/bezcirc2.pdf,
+    # via http://www.whizkidtech.redprince.net/bezier/circle/
+    class SegmentBuilder
+      attr_reader :sweep, :start, :radius
+
+      def initialize(sweep, start, radius)
+        @sweep, @start, @radius = sweep, start, radius
+      end
+
+      def first_point
+        Point.new(x0, -y0).transform(transform)
+      end
+
+      def cubic_bezier
+        CubicBezier.new({
+          end_point: end_point, control_point_1: control_point_1, control_point_2: control_point_2
+        }).transform(transform)
+      end
+
+      def transform
+        @transform ||= Transformations::Composer.compose(
+          Transformations.rotate(start + sweep_offset), Transformations.scale(radius)
+        )
+      end
+
+      def sweep_offset
+        @sweep_offset ||= sweep / 2.0
+      end
+
+      def end_point
+        Point.new(x0, y0)
+      end
+
+      def control_point_1
+        Point.new(x1, -y1)
+      end
+
+      def control_point_2
+        Point.new(x1, y1)
+      end
+
+      def x0
+        Math.cos(sweep_offset)
+      end
+
+      def y0
+        Math.sin(sweep_offset)
+      end
+
+      def x1
+        (4 - x0)/3.0
+      end
+
+      def y1
+        ((1 - x0) * (3 - x0)) / (3 * y0)
+      end
+    end
+  end
+end

spec/draught/arc_spec.rb
@@ -1,0 +1,145 @@
+require 'draught/arc'
+
+module Draught
+  RSpec.describe Arc do
+    def deg_to_rad(degrees)
+      degrees * (Math::PI / 180)
+    end
+
+    let(:degrees) { 90 }
+    let(:radians) { deg_to_rad(degrees) }
+    subject { Arc.new(radius: 100, radians: radians) }
+
+    it "defaults to a starting angle of 0 radians" do
+      expect(subject.starting_angle).to eq(0)
+    end
+
+    it "reports its total angle in radians" do
+      expect(subject.radians).to eq(radians)
+    end
+
+    it "reports its radius" do
+      expect(subject.radius).to eq(100)
+    end
+
+    context "generated path" do
+      it "generates a one-segment curve for a 90º arc" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          })
+        ])
+
+        expect(subject.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a two-segment curve for a 180º arc" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-100, 0), control_point_1: Point.new(-55.22847, 100),
+            control_point_2: Point.new(-100, 55.22847)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(180))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a three-segment curve for a 270º arc" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-100, 0), control_point_1: Point.new(-55.22847, 100),
+            control_point_2: Point.new(-100, 55.22847)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(0, -100), control_point_1: Point.new(-100, -55.22847),
+            control_point_2: Point.new(-55.22847, -100)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(270))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a four-segment curve for a 360º arc" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-100, 0), control_point_1: Point.new(-55.22847, 100),
+            control_point_2: Point.new(-100, 55.22847)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(0, -100), control_point_1: Point.new(-100, -55.22847),
+            control_point_2: Point.new(-55.22847, -100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(100, 0), control_point_1: Point.new(55.22847, -100),
+            control_point_2: Point.new(100, -55.22847)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(360))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a two-segment curve for an arc between 90 and 180º" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(100, 55.22847),
+            control_point_2: Point.new(55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-17.36482, 98.48078),
+            control_point_1: Point.new( -5.82146, 100),
+            control_point_2: Point.new(-11.63179, 99.49166)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(100))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+
+      it "generates a clockwise arc if a negative angle is used" do
+        path = Path.new([
+          Point.new(100, 0),
+          CubicBezier.new({
+            end_point: Point.new(0, -100), control_point_1: Point.new(100, -55.22847),
+            control_point_2: Point.new(55.22847, -100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(-100, 0), control_point_1: Point.new(-55.22847, -100),
+            control_point_2: Point.new(-100, -55.22847)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(0, 100), control_point_1: Point.new(-100, 55.22847),
+            control_point_2: Point.new(-55.22847, 100)
+          }),
+          CubicBezier.new({
+            end_point: Point.new(100, 0), control_point_1: Point.new(55.22847, 100),
+            control_point_2: Point.new(100, 55.22847)
+          })
+        ])
+        arc = Arc.new(radius: 100, radians: deg_to_rad(-360))
+
+        expect(arc.path).to approximate(path).within(0.00001)
+      end
+    end
+  end
+end

spec/draught/path_spec.rb
@@ -49,47 +49,40 @@ module Draught
       expect(path.points).to eq([point, other_point])
     end
 
-    it "can return the first point in the path" do
-      path = Path.new([point])
-
-      expect(path.first).to be(point)
-    end
-
-    it "can return the last point in the path" do
-      path = Path.new([point])
-
-      expect(path.last).to be(point)
-    end
-
-    it "can return the number of points in the path" do
-      path = Path.new([point])
-
-      expect(path.length).to eq(1)
-    end
-
-    describe "equality" do
+    describe "comparison" do
       let(:p1) { Point.new(1,1) }
       let(:p2) { Point.new(1,2) }
       let(:p3) { Point.new(2,1) }
 
       subject { Path.new([p1, p2]) }
 
-      it "compares two Paths equal if all their Points are equal" do
-        path = Path.new([p1, p2])
+      context "equality" do
+        it "compares two Paths equal if all their Points are equal" do
+          path = Path.new([p1, p2])
 
-        expect(subject == path).to be(true)
+          expect(subject == path).to be(true)
+        end
+
+        it "compares two Paths unequal if one has a different number of Points" do
+          path = subject << p3
+
+          expect(subject == path).to be(false)
+        end
+
+        it "compares two Paths unequal if one or more of their Points are not equal" do
+          path = Path.new([p1, p3])
+
+          expect(subject == path).to be(false)
+        end
+
+        it "compares equal to a (0,0) translation of itself" do
+          expect(subject.translate(Draught::Vector.new(0,0))).to eq(subject)
+        end
       end
 
-      it "compares two Paths unequal if one has a different number of Points" do
-        path = subject << p3
-
-        expect(subject == path).to be(false)
-      end
-
-      it "compares two Paths unequal if one or more of their Points are not equal" do
-        path = Path.new([p1, p3])
-
-        expect(subject == path).to be(false)
+      it "compares two Paths approximately equal if all their Points are approximately equal" do
+        approx_path = subject.translate(Vector.new(0.000001, 0.000001))
+        expect(subject.approximates?(approx_path, 0.00001)).to be(true)
       end
     end
 