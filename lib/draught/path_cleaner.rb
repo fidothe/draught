fidothe/box_dividers

Add PathCleaner to clean excess points from paths
Add a PathCleaner that can remove sequential duplicate points (e.g.
resulting from path segments being connected together) and that can
simplify paths by removing unnecessary intermediate points in straight
lines (alebit only horizontal or vertical straight lines).

lib/box_dividers/path_cleaner.rb
@@ -1,0 +1,77 @@
+require_relative 'path'
+
+module BoxDividers
+  class PathCleaner
+    def self.dedupe(path)
+      Path.new(new(path.points).dedupe)
+    end
+
+    def self.simplify(path)
+      Path.new(new(path.points).simplify)
+    end
+
+    attr_reader :input_points
+
+    def initialize(input_points)
+      @input_points = input_points
+    end
+
+    def dedupe
+      output_points = [input_points.first]
+      input_points.inject do |previous_point, point|
+        output_points << point if point != previous_point
+        point
+      end
+      output_points
+    end
+
+    def simplify
+      points = dedupe
+      pos = 0
+      while pos < (points.length - 2)
+        triple = points[pos, 3]
+        if intercepts?(*triple)
+          points.delete_at(pos + 1)
+        else
+          pos += 1
+        end
+      end
+      points
+    end
+
+    private
+
+    def intercepts?(previous_point, point, next_point)
+      intercepts_horizontal?(previous_point, point, next_point) ||
+        intercepts_vertical?(previous_point, point, next_point)
+    end
+
+    def intercepts_horizontal?(previous_point, point, next_point)
+      points = [previous_point, point, next_point]
+      intercepts_line?(points, :x)
+    end
+
+    def intercepts_vertical?(previous_point, point, next_point)
+      points = [previous_point, point, next_point]
+      intercepts_line?(points, :y)
+    end
+
+    def intercepts_line?(points, axis)
+      axis_aligned?(points, perpendicular_axis(axis)) && obviously_intermediate?(points, axis)
+    end
+
+    def perpendicular_axis(axis)
+      {:x => :y, :y => :x}.fetch(axis)
+    end
+
+    def axis_aligned?(points, axis)
+      points.map(&axis).uniq.length == 1
+    end
+
+    def obviously_intermediate?(points, axis)
+      p1, p2, p3 = points.map(&axis)
+      operator = p1 < p3 ? :< : :>
+      p1.send(operator, p2) && p2.send(operator, p3)
+    end
+  end
+end

spec/box_dividers/path_cleaner_spec.rb
@@ -1,0 +1,66 @@
+require 'box_dividers/path_cleaner'
+require 'box_dividers/point'
+
+module BoxDividers
+  RSpec.describe PathCleaner do
+    def p(x, y)
+      Point.new(x, y)
+    end
+
+    it "can remove extra identical points and returns a new cleaned path" do
+      input = Path.new([p(0,0), p(1,0), p(1,0), p(4,0)])
+      expected = Path.new([p(0,0), p(1,0), p(4,0)])
+
+      expect(PathCleaner.dedupe(input)).to eq(expected)
+    end
+
+    describe "simplifying a path" do
+      let(:expected) { Path.new([p(0,0), p(4,0), p(4,4)]) }
+
+      it "removes unnecessary intermediate points on the horizontal" do
+        input = Path.new([p(0,0), p(1,0), p(1,0), p(2,0), p(3,0), p(4,0), p(4,4)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "removes unnecessary intermediate points on the horizontal" do
+        input = Path.new([p(0,0), p(1,0), p(1,0), p(2,0), p(3,0), p(4,0), p(4,4)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "removes unnecessary intermediate points on the vertical" do
+        input = Path.new([p(0,0), p(4,0), p(4,1), p(4,2), p(4,4)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "goes around corners" do
+        input = Path.new([p(0,0), p(1,0), p(3,0), p(4,0), p(4,2), p(4,4)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "does not remove points where it's not obvious which point to remove" do
+        input = Path.new([p(0,0), p(4,0), p(1,0)])
+        expected = Path.new([p(0,0), p(4,0), p(1,0)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "copes with paths proceeding right-to-left" do
+        input = Path.new([p(4,0), p(2,0), p(1,0)])
+        expected = Path.new([p(4,0), p(1,0)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "copes with paths proceeding top-to-bottom" do
+        input = Path.new([p(0,4), p(0,2), p(0,1)])
+        expected = Path.new([p(0,4), p(0,1)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+    end
+  end
+end

fidothe/draught

lib/draught/path_cleaner.rb
@@ -1,0 +1,77 @@
+require_relative 'path'
+
+module Draught
+  class PathCleaner
+    def self.dedupe(path)
+      Path.new(new(path.points).dedupe)
+    end
+
+    def self.simplify(path)
+      Path.new(new(path.points).simplify)
+    end
+
+    attr_reader :input_points
+
+    def initialize(input_points)
+      @input_points = input_points
+    end
+
+    def dedupe
+      output_points = [input_points.first]
+      input_points.inject do |previous_point, point|
+        output_points << point if point != previous_point
+        point
+      end
+      output_points
+    end
+
+    def simplify
+      points = dedupe
+      pos = 0
+      while pos < (points.length - 2)
+        triple = points[pos, 3]
+        if intercepts?(*triple)
+          points.delete_at(pos + 1)
+        else
+          pos += 1
+        end
+      end
+      points
+    end
+
+    private
+
+    def intercepts?(previous_point, point, next_point)
+      intercepts_horizontal?(previous_point, point, next_point) ||
+        intercepts_vertical?(previous_point, point, next_point)
+    end
+
+    def intercepts_horizontal?(previous_point, point, next_point)
+      points = [previous_point, point, next_point]
+      intercepts_line?(points, :x)
+    end
+
+    def intercepts_vertical?(previous_point, point, next_point)
+      points = [previous_point, point, next_point]
+      intercepts_line?(points, :y)
+    end
+
+    def intercepts_line?(points, axis)
+      axis_aligned?(points, perpendicular_axis(axis)) && obviously_intermediate?(points, axis)
+    end
+
+    def perpendicular_axis(axis)
+      {:x => :y, :y => :x}.fetch(axis)
+    end
+
+    def axis_aligned?(points, axis)
+      points.map(&axis).uniq.length == 1
+    end
+
+    def obviously_intermediate?(points, axis)
+      p1, p2, p3 = points.map(&axis)
+      operator = p1 < p3 ? :< : :>
+      p1.send(operator, p2) && p2.send(operator, p3)
+    end
+  end
+end

spec/draught/path_cleaner_spec.rb
@@ -1,0 +1,66 @@
+require 'draught/path_cleaner'
+require 'draught/point'
+
+module Draught
+  RSpec.describe PathCleaner do
+    def p(x, y)
+      Point.new(x, y)
+    end
+
+    it "can remove extra identical points and returns a new cleaned path" do
+      input = Path.new([p(0,0), p(1,0), p(1,0), p(4,0)])
+      expected = Path.new([p(0,0), p(1,0), p(4,0)])
+
+      expect(PathCleaner.dedupe(input)).to eq(expected)
+    end
+
+    describe "simplifying a path" do
+      let(:expected) { Path.new([p(0,0), p(4,0), p(4,4)]) }
+
+      it "removes unnecessary intermediate points on the horizontal" do
+        input = Path.new([p(0,0), p(1,0), p(1,0), p(2,0), p(3,0), p(4,0), p(4,4)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "removes unnecessary intermediate points on the horizontal" do
+        input = Path.new([p(0,0), p(1,0), p(1,0), p(2,0), p(3,0), p(4,0), p(4,4)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "removes unnecessary intermediate points on the vertical" do
+        input = Path.new([p(0,0), p(4,0), p(4,1), p(4,2), p(4,4)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "goes around corners" do
+        input = Path.new([p(0,0), p(1,0), p(3,0), p(4,0), p(4,2), p(4,4)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "does not remove points where it's not obvious which point to remove" do
+        input = Path.new([p(0,0), p(4,0), p(1,0)])
+        expected = Path.new([p(0,0), p(4,0), p(1,0)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "copes with paths proceeding right-to-left" do
+        input = Path.new([p(4,0), p(2,0), p(1,0)])
+        expected = Path.new([p(4,0), p(1,0)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+
+      it "copes with paths proceeding top-to-bottom" do
+        input = Path.new([p(0,4), p(0,2), p(0,1)])
+        expected = Path.new([p(0,4), p(0,1)])
+
+        expect(PathCleaner.simplify(input)).to eq(expected)
+      end
+    end
+  end
+end