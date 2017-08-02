fidothe/box_dividers

Add first-cut BoundingBox to group/contain paths
Add a BoundingBox that can hold multiple paths and figure out the
upper-right and lower-left points of the group. Added the ability to
report upper-right and lower-left points to Paths too...

lib/box_dividers/bounding_box.rb
@@ -1,0 +1,49 @@
+require_relative 'point'
+
+module BoxDividers
+  class BoundingBox
+    attr_reader :paths
+
+    def initialize(*paths)
+      @paths = paths
+    end
+
+    def width
+      x_max - x_min
+    end
+
+    def height
+      y_max - y_min
+    end
+
+    def translate(point)
+      self.class.new(*paths.map { |path| path.translate(point) })
+    end
+
+    private
+
+    def x_max
+      @x_max ||= upper_rights.map(&:x).max
+    end
+
+    def x_min
+      @x_min ||= lower_lefts.map(&:x).min
+    end
+
+    def y_max
+      @y_max ||= upper_rights.map(&:y).max
+    end
+
+    def y_min
+      @y_min ||= lower_lefts.map(&:y).min
+    end
+
+    def lower_lefts
+      @lower_lefts ||= paths.map(&:lower_left)
+    end
+
+    def upper_rights
+      @upper_rights ||= paths.map(&:upper_right)
+    end
+  end
+end

spec/box_dividers/bounding_box_spec.rb
@@ -1,0 +1,28 @@
+require 'box_dividers/bounding_box'
+require 'box_dividers/path'
+
+module BoxDividers
+  RSpec.describe BoundingBox do
+    let(:input_path) { Path.new([Point.new(-1, -1), Point.new(3,3)]) }
+    subject { BoundingBox.new(input_path) }
+
+    it "returns the width of the path" do
+      expect(subject.width).to eq(4)
+    end
+
+    it "returns the height of the path" do
+      expect(subject.height).to eq(4)
+    end
+
+    it "can return its paths" do
+      expect(subject.paths).to eq([input_path])
+    end
+
+    it "can be translated" do
+      translated = subject.translate(Point.new(1,0))
+      expected = [Path.new([Point.new(0,-1), Point.new(4,3)])]
+
+      expect(translated.paths).to eq(expected)
+    end
+  end
+end

fidothe/draught

lib/draught/bounding_box.rb
@@ -1,0 +1,49 @@
+require_relative 'point'
+
+module Draught
+  class BoundingBox
+    attr_reader :paths
+
+    def initialize(*paths)
+      @paths = paths
+    end
+
+    def width
+      x_max - x_min
+    end
+
+    def height
+      y_max - y_min
+    end
+
+    def translate(point)
+      self.class.new(*paths.map { |path| path.translate(point) })
+    end
+
+    private
+
+    def x_max
+      @x_max ||= upper_rights.map(&:x).max
+    end
+
+    def x_min
+      @x_min ||= lower_lefts.map(&:x).min
+    end
+
+    def y_max
+      @y_max ||= upper_rights.map(&:y).max
+    end
+
+    def y_min
+      @y_min ||= lower_lefts.map(&:y).min
+    end
+
+    def lower_lefts
+      @lower_lefts ||= paths.map(&:lower_left)
+    end
+
+    def upper_rights
+      @upper_rights ||= paths.map(&:upper_right)
+    end
+  end
+end

spec/draught/bounding_box_spec.rb
@@ -1,0 +1,28 @@
+require 'draught/bounding_box'
+require 'draught/path'
+
+module Draught
+  RSpec.describe BoundingBox do
+    let(:input_path) { Path.new([Point.new(-1, -1), Point.new(3,3)]) }
+    subject { BoundingBox.new(input_path) }
+
+    it "returns the width of the path" do
+      expect(subject.width).to eq(4)
+    end
+
+    it "returns the height of the path" do
+      expect(subject.height).to eq(4)
+    end
+
+    it "can return its paths" do
+      expect(subject.paths).to eq([input_path])
+    end
+
+    it "can be translated" do
+      translated = subject.translate(Point.new(1,0))
+      expected = [Path.new([Point.new(0,-1), Point.new(4,3)])]
+
+      expect(translated.paths).to eq(expected)
+    end
+  end
+end