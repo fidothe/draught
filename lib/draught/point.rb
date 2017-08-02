fidothe/box_dividers

Add a very basic Point object
No idea about units, but has x and y

lib/box_dividers/point.rb
@@ -1,0 +1,9 @@
+module BoxDividers
+  class Point
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+  end
+end

spec/box_dividers/point_spec.rb
@@ -1,0 +1,15 @@
+require 'box_dividers/point'
+
+module BoxDividers
+  RSpec.describe Point do
+    subject { Point.new(1, 2) }
+
+    it "returns its x" do
+      expect(subject.x).to eq(1)
+    end
+
+    it "returns its y" do
+      expect(subject.y).to eq(2)
+    end
+  end
+end

fidothe/draught

lib/draught/point.rb
@@ -1,0 +1,9 @@
+module Draught
+  class Point
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+  end
+end

spec/draught/point_spec.rb
@@ -1,0 +1,15 @@
+require 'draught/point'
+
+module Draught
+  RSpec.describe Point do
+    subject { Point.new(1, 2) }
+
+    it "returns its x" do
+      expect(subject.x).to eq(1)
+    end
+
+    it "returns its y" do
+      expect(subject.y).to eq(2)
+    end
+  end
+end