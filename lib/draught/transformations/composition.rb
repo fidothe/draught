fidothe/box_dividers

Extract a Composition object from Composer
Extract a Composition object that just contains the transforms that have
been composed and the knowledge to correctly be #call()ed.

In doing that, Composer gets a lot simpler - we can assume that
Composition only ever contains flattened and coalesced transforms, so we
can lose the whole flattened_transforms thing from
Transformations::Common and Composition.

Make use of #to_transform in the Composer process so that we can think
about using objects like Vector which can return an Affine transform
version of themselves directly. Moar polymorphism.

Rework the shared transformation examples a little to reflect these new
changes.

Also, remove #compose from Transformations::Common because it was a
latent circular dependency that became unlatent once Composition was
extracted.

lib/box_dividers/transformations/composition.rb
@@ -1,0 +1,31 @@
+require_relative './common'
+
+module BoxDividers
+  module Transformations
+    class Composition
+      include Transformations::Common
+
+      attr_reader :transforms
+
+      def initialize(transforms)
+        @transforms = transforms
+      end
+
+      def call(point)
+        transforms.inject(point) { |point, transform| transform.call(point) }
+      end
+
+      def affine?
+        false
+      end
+
+      def to_transform
+        self
+      end
+
+      def ==(other)
+        other.transforms == transforms
+      end
+    end
+  end
+end

fidothe/draught

lib/draught/transformations/composition.rb
@@ -1,0 +1,31 @@
+require_relative './common'
+
+module Draught
+  module Transformations
+    class Composition
+      include Transformations::Common
+
+      attr_reader :transforms
+
+      def initialize(transforms)
+        @transforms = transforms
+      end
+
+      def call(point)
+        transforms.inject(point) { |point, transform| transform.call(point) }
+      end
+
+      def affine?
+        false
+      end
+
+      def to_transform
+        self
+      end
+
+      def ==(other)
+        other.transforms == transforms
+      end
+    end
+  end
+end