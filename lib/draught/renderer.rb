fidothe/box_dividers

Add Renderer to properly draw a Sheet
Normalise the naming around things inside other things, so that
everything has a #paths array and a #containers array. This interface
driven by the Renderer class and a desire to be pretty flexible -
containers can contain other containers or paths and the renderer
shouldn't balk.

lib/box_dividers/renderer.rb
@@ -1,0 +1,79 @@
+require 'prawn'
+
+module BoxDividers
+  class Renderer
+    class PdfContext
+      include Prawn::View
+
+      attr_reader :width, :height
+
+      def initialize(width, height)
+        @width, @height = width, height
+      end
+
+      def document
+        @document ||= Prawn::Document.new(page_size: [width, height], margin: 0)
+      end
+
+      def draw_closed_path(path)
+        points = path.points.dup
+        first_point = points.shift
+        close_and_stroke do
+          self.line_width = 0.003
+          move_to first_point.x, first_point.y
+          points.each do |point|
+            line_to point.x, point.y
+          end
+          line_to first_point.x, first_point.y
+        end
+      end
+    end
+
+    def self.render_to_file(sheet, path)
+      new(sheet).render_to_file(path)
+    end
+
+    attr_reader :sheet
+
+    def initialize(sheet)
+      @sheet = sheet
+    end
+
+    def context
+      @context ||= PdfContext.new(sheet.width, sheet.height)
+    end
+
+    def render_to_file(path)
+      render && context.save_as(path)
+    end
+
+    def render
+      sheet.containers.each do |container|
+        walk(container)
+      end
+    end
+
+    def render_container(container, context)
+    end
+
+    def render_path(path, context)
+      context.draw_closed_path(path)
+    end
+
+    private
+
+    def walk(container)
+      render_container(container, context)
+      render_paths(container.paths, context)
+      container.containers.each do |container|
+        walk(container)
+      end
+    end
+
+    def render_paths(paths, context)
+      paths.each do |path|
+        render_path(path, context)
+      end
+    end
+  end
+end

spec/box_dividers/renderer_spec.rb
@@ -1,0 +1,54 @@
+require 'box_dividers/renderer'
+require 'box_dividers/point'
+require 'box_dividers/path_builder'
+require 'box_dividers/container'
+require 'box_dividers/sheet_builder'
+require 'tmpdir'
+
+module BoxDividers
+  RSpec.describe Renderer do
+    let(:path) { PathBuilder.build { |p| p << Point.new(0,0); p << Point.new(50,50) } }
+    let(:container) { Container.new(path, min_gap: 50) }
+    let(:sheet) {
+      box = container
+      SheetBuilder.build(max_width: 150, max_height: 150) { add box }
+    }
+    subject { Renderer.new(sheet) }
+
+    specify "Boxes in the sheet are passed to the render_container method with the render context" do
+      context = subject.context
+      sheet.containers.each do |box|
+        expect(subject).to receive(:render_container).with(box, context)
+      end
+
+      subject.render
+    end
+
+    specify "Paths in a container are passed to the render_path method with the render context" do
+      context = subject.context
+      sheet.containers.flat_map { |box| box.paths }.each  do |path|
+        expect(subject).to receive(:render_path).with(path, context)
+      end
+
+      subject.render
+    end
+
+    specify "the Sheet can be rendered to a file" do
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, 'tmp.pdf')
+        subject.render_to_file(path)
+
+        expect(File.file?(path)).to be(true)
+      end
+    end
+
+    it "provides a convenience class method for rendering a sheet to a file" do
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, 'tmp.pdf')
+        Renderer.render_to_file(sheet, path)
+
+        expect(File.file?(path)).to be(true)
+      end
+    end
+  end
+end

fidothe/draught

lib/draught/renderer.rb
@@ -1,0 +1,79 @@
+require 'prawn'
+
+module Draught
+  class Renderer
+    class PdfContext
+      include Prawn::View
+
+      attr_reader :width, :height
+
+      def initialize(width, height)
+        @width, @height = width, height
+      end
+
+      def document
+        @document ||= Prawn::Document.new(page_size: [width, height], margin: 0)
+      end
+
+      def draw_closed_path(path)
+        points = path.points.dup
+        first_point = points.shift
+        close_and_stroke do
+          self.line_width = 0.003
+          move_to first_point.x, first_point.y
+          points.each do |point|
+            line_to point.x, point.y
+          end
+          line_to first_point.x, first_point.y
+        end
+      end
+    end
+
+    def self.render_to_file(sheet, path)
+      new(sheet).render_to_file(path)
+    end
+
+    attr_reader :sheet
+
+    def initialize(sheet)
+      @sheet = sheet
+    end
+
+    def context
+      @context ||= PdfContext.new(sheet.width, sheet.height)
+    end
+
+    def render_to_file(path)
+      render && context.save_as(path)
+    end
+
+    def render
+      sheet.containers.each do |container|
+        walk(container)
+      end
+    end
+
+    def render_container(container, context)
+    end
+
+    def render_path(path, context)
+      context.draw_closed_path(path)
+    end
+
+    private
+
+    def walk(container)
+      render_container(container, context)
+      render_paths(container.paths, context)
+      container.containers.each do |container|
+        walk(container)
+      end
+    end
+
+    def render_paths(paths, context)
+      paths.each do |path|
+        render_path(path, context)
+      end
+    end
+  end
+end

spec/draught/renderer_spec.rb
@@ -1,0 +1,54 @@
+require 'draught/renderer'
+require 'draught/point'
+require 'draught/path_builder'
+require 'draught/container'
+require 'draught/sheet_builder'
+require 'tmpdir'
+
+module Draught
+  RSpec.describe Renderer do
+    let(:path) { PathBuilder.build { |p| p << Point.new(0,0); p << Point.new(50,50) } }
+    let(:container) { Container.new(path, min_gap: 50) }
+    let(:sheet) {
+      box = container
+      SheetBuilder.build(max_width: 150, max_height: 150) { add box }
+    }
+    subject { Renderer.new(sheet) }
+
+    specify "Boxes in the sheet are passed to the render_container method with the render context" do
+      context = subject.context
+      sheet.containers.each do |box|
+        expect(subject).to receive(:render_container).with(box, context)
+      end
+
+      subject.render
+    end
+
+    specify "Paths in a container are passed to the render_path method with the render context" do
+      context = subject.context
+      sheet.containers.flat_map { |box| box.paths }.each  do |path|
+        expect(subject).to receive(:render_path).with(path, context)
+      end
+
+      subject.render
+    end
+
+    specify "the Sheet can be rendered to a file" do
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, 'tmp.pdf')
+        subject.render_to_file(path)
+
+        expect(File.file?(path)).to be(true)
+      end
+    end
+
+    it "provides a convenience class method for rendering a sheet to a file" do
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, 'tmp.pdf')
+        Renderer.render_to_file(sheet, path)
+
+        expect(File.file?(path)).to be(true)
+      end
+    end
+  end
+end